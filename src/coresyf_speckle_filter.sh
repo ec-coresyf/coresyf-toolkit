23327f75e5bbf922f1181d9b04693203716abd1a
Nuno Costa <nuno.costa@deimos.com.pt>
2017-01-26 11:05:05 +0000

    arguments quoted

--- src/coresyf_speckle_filter.sh
+++ src/coresyf_speckle_filter.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
-
-args=`echo "$*" | sed 's/-\(\S*\)\s\+\([^- ]\S*\)/-\1=\2/g'`
+#!/bin/bash 
+args=`sed 's/-\(\S\+\)\(\(\s\+[^- ]\S\+\)\+\)/-\1="\2"/g' <<< $@`
+args=`sed 's/"\s*\(\S\+\)\s*"/"\1"/g' <<< $args`
+echo "$args"
 gpt Speckle-Filter $args
