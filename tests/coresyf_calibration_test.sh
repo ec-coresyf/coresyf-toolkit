72c11fcd105db2037bc4c30d254ebaf06a19cb08
Nuno Costa <nuno.costa@deimos.com.pt>
2017-01-27 18:43:13 +0000

    test improved
    Former-commit-id: d44fa82e7c6ad5633e2a3baa6d8ad22417e657e7

--- tests/coresyf_calibration_test.sh
+++ tests/coresyf_calibration_test.sh
@@ -1,23 +1,15 @@
 #/bin/bash 
 
-check(){
-	echo "--------------------------------------------------------------------------------"
-	echo -n "$1: " 
-	if $2; then
-		echo -e "\e[32mOK\e[39m"
-	else 
-		echo -e "\e[31mFAILED\e[39m";
-	fi
-	echo "********************************************************************************"
-	echo ""
-}
+source ./helpers.sh
 
 cd ..
 
 #gets the test file 
-wget ftp://rsat2:yvr578MM@ftp.mda.ca/Vancouver%20Dataset/Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC.zip 
-unzip Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC.zip
-mv RS2_OK871_PK6633_DK3208_FQ2_20080415_143805_HH_VV_HV_VH_SLC Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC
+if [ ! -d Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC ]; then
+	wget ftp://rsat2:yvr578MM@ftp.mda.ca/Vancouver%20Dataset/Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC.zip 
+	unzip Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC.zip
+	mv RS2_OK871_PK6633_DK3208_FQ2_20080415_143805_HH_VV_HV_VH_SLC Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC
+fi
 
 #Nominal cases:
 
