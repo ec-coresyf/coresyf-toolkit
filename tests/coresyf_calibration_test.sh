62eb669b0aee9f9edaa704a9e2ab42dac7b5699f
Nuno Costa <nuno.costa@deimos.com.pt>
2017-01-27 17:45:31 +0000

    tool automatically tested

---
+++ tests/coresyf_calibration_test.sh
@@ -0,0 +1,44 @@
+#/bin/bash 
+
+check(){
+	echo "--------------------------------------------------------------------------------"
+	echo -n "$1: " 
+	if $2; then
+		echo -e "\e[32mOK\e[39m"
+	else 
+		echo -e "\e[31mFAILED\e[39m";
+	fi
+	echo "********************************************************************************"
+	echo ""
+}
+
+cd ..
+
+#gets the test file 
+wget ftp://rsat2:yvr578MM@ftp.mda.ca/Vancouver%20Dataset/Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC.zip 
+unzip Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC.zip
+mv RS2_OK871_PK6633_DK3208_FQ2_20080415_143805_HH_VV_HV_VH_SLC Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC
+
+#Nominal cases:
+
+src/coresyf_calibration.py --Ssource Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC/product.xml
+check "source file" `[ $? -a -f target.dim ]`
+rm -f target.dim.tif
+
+src/coresyf_calibration.py --Ssource Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC/product.xml --Ttarget out
+check "target specified" `[ $? -a -f out.tif ]`
+rm -f out.tif
+
+src/coresyf_calibration.py --Ssource Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC/product.xml --Ttarget out --PauxFile 'Latest Auxiliary File' --PcreateBetaBand true --PcreateGammaBand true --PoutputBetaBand true --PoutputGammaBand true --PoutputImageInComplex true --PoutputImageScaleInDb true --PoutputSigmaBand true
+check "all parameters" `[ $? -a -f out.dim ]`
+rm -f out.tif
+
+#Error cases:
+src/coresyf_calibration.py
+check "source missing" `[ ! $? ]`
+
+src/coresyf_calibration.py --Ssource "prod.tif" 
+check "missing file" `[ ! $? ]`
+
+src/coresyf_calibration.py --Ssource Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC/product.xml --PauxFile 'prodxpto'
+check "source missing" `[ ! $? ]`
