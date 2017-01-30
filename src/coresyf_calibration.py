3238e60c1ce1fcb431b1ff7f3c49690f9e5ee7f4
Nuno Costa <nuno.costa@deimos.com.pt>
2017-01-30 15:20:35 +0000

    ProductSelector extracted

--- src/coresyf_calibration.py
+++ src/coresyf_calibration.py
@@ -16,13 +16,13 @@
 # ==============================================================================
 
 """ SYSTEM MODULES """
-import fnmatch
-import glob
 import os
 import subprocess
 import zipfile
 from argparse import ArgumentParser
 from os import path
+
+from product_selector import ProductSelector
 
 '''
 @summary: 
@@ -172,50 +172,6 @@ def main():
     for i in product_files:
         print ("Applying calibration %s..." % i)
         call_gpt('Calibration', ps.openproduct(i), target, opts)
-
-
-class ProductSelector:
-    def __init__(self, glob_selector):
-        self.selector = glob_selector
-
-    def isproduct(self, filepath):
-        if not os.path.exists(filepath):
-            raise Exception("File not found")
-        if zipfile.is_zipfile(filepath):
-            with zipfile.ZipFile(filepath, 'r') as z:
-                namelist = [n for n in z.namelist() if fnmatch.fnmatch(n, self.selector)]
-                if len(namelist) > 1:
-                    raise Exception("Invalid product selector or product archive: more than one file is being selected")
-                return len(namelist) == 1
-        elif os.path.isfile(filepath):
-            return True
-        elif os.path.isdir(filepath):
-            files = glob.glob(os.path.join(filepath, self.selector))
-            if len(files) > 1:
-                raise Exception("Invalid product selector or product archive: more than one file is being selected")
-            return len(files) == 1
-
-    def openproduct(self, filepath):
-        if not os.path.exists(filepath):
-            raise Exception("File not found")
-        if zipfile.is_zipfile(filepath):
-            with zipfile.ZipFile(filepath, 'r') as z:
-                namelist = [n for n in z.namelist() if fnmatch.fnmatch(n, self.selector)]
-                if not namelist:
-                    raise Exception("Invalid product selector or product archive: no file is being selected")
-                if len(namelist) > 1:
-                    raise Exception("Invalid product selector or product archive: more than one file is being selected")
-                z.extractall()
-                return namelist[0]
-        elif os.path.isfile(filepath):
-            return filepath
-        elif os.path.isdir(filepath):
-            files = glob.glob(os.path.join(filepath, self.selector))
-            if not files:
-                raise Exception("Invalid product selector or product archive: no file is being selected")
-            if len(files) > 1:
-                raise Exception("Invalid product selector or product archive: more than one file is being selected")
-            return files[0]
 
 
 def parameter(prefix, value):
