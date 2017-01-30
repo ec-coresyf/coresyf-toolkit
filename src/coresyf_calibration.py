bcb1480b2cb5f461339986a1b78ec25c19866671
Nuno Costa <nuno.costa@deimos.com.pt>
2017-01-30 15:12:45 +0000

    source accepts zip file

--- src/coresyf_calibration.py
+++ src/coresyf_calibration.py
@@ -16,12 +16,13 @@
 # ==============================================================================
 
 """ SYSTEM MODULES """
+import fnmatch
+import glob
+import os
 import subprocess
-import sys
+import zipfile
 from argparse import ArgumentParser
 from os import path
-import os
-import glob
 
 '''
 @summary: 
@@ -73,6 +74,7 @@ VERSION = '1.0'
 
 DefaultAuxFilesLookup = ['Latest Auxiliary File', 'Product Auxiliary File', 'External Auxiliary File']
 
+
 def main():
     parser = ArgumentParser(version=VERSION)
 
@@ -80,8 +82,8 @@ def main():
     # Define command line options  #
     # ==============================#
     parser.add_argument('--Ssource',
-                      dest="Ssource", metavar='<filepath>',
-                      help="Sets source to <filepath>",
+                        dest="Ssource", metavar='<filepath>',
+                        help="Sets source to <filepath>",
                         required=True)
     parser.add_argument('--Pselector',
                         dest="Pselector", metavar='<glob>',
@@ -90,55 +92,55 @@ def main():
                         dest="Ttarget",
                         help="Sets the target to <filepath>")
     parser.add_argument('--PauxFile',
-                      dest="PauxFile", metavar='<string>',
-                      help="Value must be one of 'Latest Auxiliary File', 'Product Auxiliary File', "
-                           "'External Auxiliary File'.",
-                      default="Latest Auxiliary File",
+                        dest="PauxFile", metavar='<string>',
+                        help="Value must be one of 'Latest Auxiliary File', 'Product Auxiliary File', "
+                             "'External Auxiliary File'.",
+                        default="Latest Auxiliary File",
                         choices=DefaultAuxFilesLookup)
     parser.add_argument('--PcreateBetaBand',
-                      dest="PcreateBetaBand", metavar='<boolean>',
-                      help="Create beta0 virtual band.",
-                      type=bool,
-                      default=False)
+                        dest="PcreateBetaBand", metavar='<boolean>',
+                        help="Create beta0 virtual band.",
+                        type=bool,
+                        default=False)
     parser.add_argument('--PcreateGammaBand',
-                      dest="PcreateGammaBand", metavar='<boolean>',
-                      help="Create gamma0 virtual band.",
-                      default=False,
-                      type=bool)
+                        dest="PcreateGammaBand", metavar='<boolean>',
+                        help="Create gamma0 virtual band.",
+                        default=False,
+                        type=bool)
     parser.add_argument('--PexternalAuxFile',
-                      dest="PexternalAuxFile", metavar='<file>',
-                      help="The antenna elevation pattern gain auxiliary data file.")
+                        dest="PexternalAuxFile", metavar='<file>',
+                        help="The antenna elevation pattern gain auxiliary data file.")
     parser.add_argument('--PoutputBetaBand',
-                      dest="PoutputBetaBand", metavar='<boolean>',
-                      help="Output beta0 band.",
-                      type=bool,
-                      default=False)
+                        dest="PoutputBetaBand", metavar='<boolean>',
+                        help="Output beta0 band.",
+                        type=bool,
+                        default=False)
     parser.add_argument('--PoutputGammaBand',
-                      dest="PoutputGammaBand", metavar='<boolean>',
-                      help="Output gamma0 band.",
-                      type=bool,
-                      default=False)
+                        dest="PoutputGammaBand", metavar='<boolean>',
+                        help="Output gamma0 band.",
+                        type=bool,
+                        default=False)
     parser.add_argument('--PoutputImageInComplex',
-                      dest="PoutputImageInComplex", metavar='<boolean>',
-                      help="Output image in complex.",
-                      type=bool,
-                      default=False)
+                        dest="PoutputImageInComplex", metavar='<boolean>',
+                        help="Output image in complex.",
+                        type=bool,
+                        default=False)
     parser.add_argument('--PoutputImageScaleInDb',
-                      dest="PoutputImageScaleInDb", metavar='<boolean>',
-                      help="Output image scale.",
-                      type=bool,
-                      default=False)
+                        dest="PoutputImageScaleInDb", metavar='<boolean>',
+                        help="Output image scale.",
+                        type=bool,
+                        default=False)
     parser.add_argument("--PoutputSigmaBand", metavar='<boolean>',
-                      dest="PoutputSigmaBand",
-                      help="Output sigma0 band.",
-                      type=bool,
-                      default=True)
+                        dest="PoutputSigmaBand",
+                        help="Output sigma0 band.",
+                        type=bool,
+                        default=True)
     parser.add_argument("--PselectedPolarisations", metavar='<string,string,string,...>',
-                      dest="PselectedPolarisations",
-                      help="The list of polarisations.")
+                        dest="PselectedPolarisations",
+                        help="The list of polarisations.")
     parser.add_argument("--PsourceBands", metavar='<string,string,string,...>',
-                      dest="PsourceBands",
-                      help="The list of source bands.")
+                        dest="PsourceBands",
+                        help="The list of source bands.")
 
     opts = vars(parser.parse_args())
 
@@ -148,31 +150,77 @@ def main():
 
     if not os.path.exists(source):
         parser.error("%s does not exists." % source)
-    if path.isfile(source) and selector:
-        parser.error("Selectors should be used only for sources which are directories.")
+    if path.isfile(source) and selector and not zipfile.is_zipfile(source):
+        parser.error("Selectors should be used only for sources which are directories or zips.")
     if path.isdir(source) and not selector:
         parser.error("Selector parameter is missing.")
     if path.isdir(source) and target:
         parser.error("Target should not be specified when multiple source files are selected from a dir.")
 
+    ps = ProductSelector(selector)
+
     # ====================================#
     #  LOOP THROUGH ALL SELECTED PRODUCTS#
     # ====================================#
-    product_files = []
     if path.isdir(source):
-        product_files = [file for file in glob.glob(source + path.sep + selector)]
+        product_files = [path.join(source, fname) for fname in os.listdir(source)
+                         if ps.isproduct(path.join(source, fname))]
         print("selected files: %s%s" % (os.linesep, os.linesep.join(product_files)))
     else:
         product_files = [source]
 
     for i in product_files:
         print ("Applying calibration %s..." % i)
-        call_gpt('Calibration', i, target, opts)
+        call_gpt('Calibration', ps.openproduct(i), target, opts)
+
+
+class ProductSelector:
+    def __init__(self, glob_selector):
+        self.selector = glob_selector
+
+    def isproduct(self, filepath):
+        if not os.path.exists(filepath):
+            raise Exception("File not found")
+        if zipfile.is_zipfile(filepath):
+            with zipfile.ZipFile(filepath, 'r') as z:
+                namelist = [n for n in z.namelist() if fnmatch.fnmatch(n, self.selector)]
+                if len(namelist) > 1:
+                    raise Exception("Invalid product selector or product archive: more than one file is being selected")
+                return len(namelist) == 1
+        elif os.path.isfile(filepath):
+            return True
+        elif os.path.isdir(filepath):
+            files = glob.glob(os.path.join(filepath, self.selector))
+            if len(files) > 1:
+                raise Exception("Invalid product selector or product archive: more than one file is being selected")
+            return len(files) == 1
+
+    def openproduct(self, filepath):
+        if not os.path.exists(filepath):
+            raise Exception("File not found")
+        if zipfile.is_zipfile(filepath):
+            with zipfile.ZipFile(filepath, 'r') as z:
+                namelist = [n for n in z.namelist() if fnmatch.fnmatch(n, self.selector)]
+                if not namelist:
+                    raise Exception("Invalid product selector or product archive: no file is being selected")
+                if len(namelist) > 1:
+                    raise Exception("Invalid product selector or product archive: more than one file is being selected")
+                z.extractall()
+                return namelist[0]
+        elif os.path.isfile(filepath):
+            return filepath
+        elif os.path.isdir(filepath):
+            files = glob.glob(os.path.join(filepath, self.selector))
+            if not files:
+                raise Exception("Invalid product selector or product archive: no file is being selected")
+            if len(files) > 1:
+                raise Exception("Invalid product selector or product archive: more than one file is being selected")
+            return files[0]
 
 
 def parameter(prefix, value):
-    format = ("-%s=\"%s\"" if isinstance(value, basestring) else "-%s=%s")
-    return format % (prefix, value)
+    f = ("-%s=\"%s\"" if isinstance(value, basestring) else "-%s=%s")
+    return f % (prefix, value)
 
 
 def call_gpt(operator, source, target, options):
@@ -201,7 +249,7 @@ def call_gpt(operator, source, target, options):
             raise Exception()
     except Exception, message:
         print(str(message))
-        #sys.exit(1)  # or sys.exit(process.returncode)
+        # sys.exit(1)  # or sys.exit(process.returncode)
 
 
 if __name__ == '__main__':
