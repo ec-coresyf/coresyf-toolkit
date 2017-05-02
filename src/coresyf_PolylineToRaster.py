cf486fe070c59c4cef79e37d16c06801263c5edf
rccc <ricardo.capote@deimos.com.pt>
2017-05-02 10:57:25 +0100

    Minor corrections on the tools code and comments.
    Former-commit-id: 13b8c9f7209777b9c07fc8b3cc1f4f364370f73c

--- src/coresyf_PolylineToRaster.py
+++ src/coresyf_PolylineToRaster.py
@@ -7,7 +7,7 @@
 # Component : Co-ReSyF Tools (Polyline to Raster)
 # Language  : Python (v.2.6)
 #------------------------------------------------------------------------------
-# Scope : Command line polyline to raster tool for GDAL supported files
+# Scope : Command line tool for converting a polyline shapefile into a raster
 # Usage : (see the following docstring)
 #==============================================================================
 # $LastChangedRevision:  $:
@@ -19,15 +19,25 @@
 @summary:
 This module runs the following Co-ReSyF tool:
  - Polyline to Raster
-It uses the GDAL raster utility program "gdal_rasterize" to burn vector geometries into a raster. 
-This program burns vector geometries (points, lines, and polygons) into the raster band(s) of a 
+It uses the GDAL raster utility program "gdal_rasterize" to convert vector geometries into a raster. 
+This program converts vector geometries (points, lines, and polygons) into the raster band(s) of a 
 raster image. Vectors are read from OGR supported vector formats.
+
+List of some possible raster formats:
+GTiff:     GeoTIFF
+HFA:       Erdas Imagine Images (.img)
+BMP:       MS Windows Device Independent Bitmap
+netCDF:    Network Common Data Format
+HDF4Image: HDF4 Dataset
+
 
 @example:
 
 Example 1 - Generate a 3000 by 3000 raster with HDF4 format from a polyline:
-./coresyf_PolylineToRaster.py -n DN --width 3000 --height 3000 --o_format HDF4Image 
--r ../examples/PolylineToRaster/output_polyline.shp -l output_polyline -o output_raster.tif
+./coresyf_PolylineToRaster.py -r ../examples/PolylineToRaster/polyline.shp -l polyline -n DN -o output_raster.tif --width 3000 --height 3000 --o_format HDF4Image
+
+Example 2 - Generate a 2000 by 2000 raster with GTiff format from a polyline:
+./coresyf_PolylineToRaster.py -r ../examples/PolylineToRaster/polyline.shp -l polyline -n DN -o output_raster.tif --width 2000 --height 2000 --o_format GTiff
 
 
 @version: v.1.0
@@ -40,12 +50,11 @@ Example 1 - Generate a 3000 by 3000 raster with HDF4 format from a polyline:
 
 VERSION = '1.0'
 USAGE = ('\n'
-         'coresyf_PolylineToRaster.py [-n <FieldName>] [--width <Width>] [--height <Height>]'
-         "[--o_format <OutputFormat>] [-r <InputPolyline>] [-l <Layer>] [-o <OutputRaster>] "
+         'coresyf_PolylineToRaster.py [-r <InputPolyline>] [-l <Layer>] [-n <FieldName>]'
+         "                            [-o <OutputRaster>] [--width <Width>] [--height <Height>] [--o_format <OutputFormat>]"
  #        "[--o_type=<DataType>]"
          "\n")
 
-#DefaultTypesLookup = ['Byte','UInt16','Int16','UInt32','Int32','Float32','Float64']
 
 
 ''' SYSTEM MODULES '''
@@ -53,8 +62,6 @@ import subprocess
 import sys
 from optparse import OptionParser
 import os
-
-os.path.basename
 
 from osgeo import gdal
 
@@ -91,13 +98,13 @@ def main():
                       "(default: filename)")
     parser.add_option('--o_format',
                       dest="output_format", metavar=' ',
-                      help="GDAL format for output file (default: 'GeoTIFF')",
+                      help=("GDAL format for output file, some possible formats are "
+                            "GTiff, HDF4Image, HFA, netCDF (default: 'GTiff')"),
                       default="GTiff")
 
     #==============================#
     #   Check mandatory options    #
     #==============================#
-
     (opts, args) = parser.parse_args()
 
     if len(sys.argv) == 1:
@@ -110,19 +117,19 @@ def main():
     if not opts.layer:
         #If no layer name is received, use the input filename
         opts.layer=os.path.split(opts.input_polyline.split('.')[0])[1]
-        return
 
-        #--------------------------------------#
-        # Building gdal_translate command line #
-        #--------------------------------------#
+
+    #--------------------------------------#
+    # Building gdal_translate command line #
+    #--------------------------------------#
     gdal_exe = 'gdal_rasterize '
     output_opts = '-a %s -of %s -ts %s %s -l %s %s %s' % (opts.fieldname,
-                                                 opts.output_format,
-                                                 opts.width,
-                                                 opts.height,
-                                                 opts.layer,
-                                                 opts.input_polyline,
-                                                 opts.output_raster)
+                                                          opts.output_format,
+                                                          opts.width,
+                                                          opts.height,
+                                                          opts.layer,
+                                                          opts.input_polyline,
+                                                          opts.output_raster)
     gdal_rasterize_command = gdal_exe + output_opts
 
 
