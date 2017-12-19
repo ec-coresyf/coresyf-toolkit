5c62a215a1fbb04f8877dd248e171032fdff8589
Nuno Costa <nuno.costa@deimos.com.pt>
2017-12-19 18:51:12 +0000

    [TSK#590] improves coresyf_calibration test bash - receives input file as argument - changes some commads being tested
    Former-commit-id: c12edbcf314917052b1b1c91ffccc878de5aee32 [formerly f8eebc1978b33fefae0db258e1c2295134efed64] [formerly 8f17b7d3f3dfa836dbcc57d1a56dd44ca176a636 [formerly 160d1842d11a77607746bf64557ac6d446614ca3]] [formerly 23b6c173a3a09cb5c6a0c6c568c5d5e1646ecba4] [formerly cf6564688576547bb1b7c5a694cead86b5f35732 [formerly 40bd532a1d06a25556e0b325d5f610e3407e05c9] [formerly 6be72923816433f03a0ab5054ad4c111ef2f01a4] [formerly 2fd232b2b42a19ec25efadcc4ba1368b88722660 [formerly 79071d67bbc07e10a5cc1d1c876de015924a3b50] [formerly 275ffc10561d2d002d658541243e5e10fd79e3e0 [formerly 9db18071dd9fe9174898007c4ddc42afedead730]] [formerly d9367f4f548aababbda1e1eb5a399ebb7fc19902] [formerly cf6564688576547bb1b7c5a694cead86b5f35732 [formerly 40bd532a1d06a25556e0b325d5f610e3407e05c9] [formerly 6be72923816433f03a0ab5054ad4c111ef2f01a4] [formerly cf6564688576547bb1b7c5a694cead86b5f35732 [formerly 40bd532a1d06a25556e0b325d5f610e3407e05c9] [formerly 6be72923816433f03a0ab5054ad4c111ef2f01a4] [formerly d9367f4f548aababbda1e1eb5a399ebb7fc19902 [formerly 23b6c173a3a09cb5c6a0c6c568c5d5e1646ecba4 [formerly a7db2e20e114524d6c9b6ae86f728768463a4da6]]]]]]]
    Former-commit-id: 2fd232b2b42a19ec25efadcc4ba1368b88722660 [formerly 79071d67bbc07e10a5cc1d1c876de015924a3b50] [formerly 275ffc10561d2d002d658541243e5e10fd79e3e0 [formerly 9db18071dd9fe9174898007c4ddc42afedead730]] [formerly 2fd232b2b42a19ec25efadcc4ba1368b88722660 [formerly 79071d67bbc07e10a5cc1d1c876de015924a3b50] [formerly 275ffc10561d2d002d658541243e5e10fd79e3e0 [formerly 9db18071dd9fe9174898007c4ddc42afedead730]] [formerly d9367f4f548aababbda1e1eb5a399ebb7fc19902]]
    Former-commit-id: 48ec7c7b65c2c10a234beac798823e189c2a822a
    Former-commit-id: d86a1920b61913c158d0d2d1cb4949567c736b2f [formerly 9e48962d395492ddd2d49937c9f5f0269ff63a1d]
    Former-commit-id: df79b66fac1bf9c22120b0b897d74ed6e39062e9
    Former-commit-id: 7be4e0148e2183afec73db391462a64442b6fed6 [formerly d798f8d8cdf62989547b80143c72e0c55f9f8c13]
    Former-commit-id: d3d230b3ecbdd6595d0466a47e0c2fd5965dfe43
    Former-commit-id: a6aae88cc16207d5e5cd684395185f9e1bae7f67

--- tests/coresyf_calibration_test.sh
+++ tests/coresyf_calibration_test.sh
@@ -1,39 +1,42 @@
 #/bin/bash 
 
-source ./helpers.sh
+source `dirname $0`/helpers.sh
 
-cd ..
+INFILE=$1
 
-#gets the test file 
-if [ ! -f Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC.zip ]; then
-	wget ftp://rsat2:yvr578MM@ftp.mda.ca/Vancouver%20Dataset/Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC.zip 
-	unzip Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC.zip
-	mv RS2_OK871_PK6633_DK3208_FQ2_20080415_143805_HH_VV_HV_VH_SLC Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC
+if [ -z "$INFILE" ]; then
+	echo 'An input file should be passed as parameter'
+	exit 1
 fi
+
+
+if [ ! -f $INFILE ]; then
+	echo 'Input file not found'
+	exit 1
+fi
+
+export _JAVA_OPTIONS="-Xmx4g"
 
 #Nominal cases:
 
-src/coresyf_calibration.py --Ssource Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC/product.xml
-test -f target.dim.tif
-check "source file" 
-rm -f target.dim.tif
-
-src/coresyf_calibration.py --Ssource Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC.zip --Pselector '*/product.xml' 
-test -f target.dim.tif
-check "source zip file"
-rm -f target.dim.tif
-
-src/coresyf_calibration.py --Ssource Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC/product.xml --Ttarget out
-test -f out.tif
+src/coresyf_calibration.py --Ssource $INFILE.SAFE  --Ttarget out
+test -f out
 check "target specified"
-rm -f out.tif
+rm -f out
 
-src/coresyf_calibration.py --Ssource Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC/product.xml --Ttarget out --PauxFile 'Latest Auxiliary File' --PcreateBetaBand true --PcreateGammaBand true --PoutputBetaBand true --PoutputGammaBand true --PoutputImageInComplex true --PoutputImageScaleInDb true --PoutputSigmaBand true --PsourceBands 'i_HH,q_HV,i_VH,q_VH' --PselectedPolarisations 'HH,HV'
-test -f out.tif
+src/coresyf_calibration.py --Ssource $INFILE.SAFE --Ttarget out --PauxFile 'Latest Auxiliary File' --PcreateBetaBand true --PcreateGammaBand true --PoutputBetaBand true --PoutputGammaBand true --PoutputImageInComplex true --PoutputImageScaleInDb true --PoutputSigmaBand true 
+##--PsourceBands 'HH'
+test -f out
 check "all parameters"
-rm -f out.tif
+rm -f out
 
 #Error cases:
+src/coresyf_calibration.py --Ssource $INFILE.SAFE
+test $? -ne 0
+check "target missing"
+rm -f target.dim.tif
+
+
 src/coresyf_calibration.py
 test $? -ne 0
 check "source missing"
@@ -43,6 +46,6 @@ src/coresyf_calibration.py --Ssource "prod.tif"
 test $? -ne 0
 check "missing file"
 
-src/coresyf_calibration.py --Ssource Vancouver_RS2_FineQuad2_HH_VV_HV_VH_SLC/product.xml --PauxFile 'prodxpto'
+src/coresyf_calibration.py --Ssource $INFILE.SAFE --PauxFile 'prodxpto'
 test $? -ne 0
 check "source missing"
