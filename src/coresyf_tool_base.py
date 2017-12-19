63bc094f09507fcfbb1990dfbffeea1fd6a1439b
Nuno Costa <nuno.costa@deimos.com.pt>
2017-12-19 18:05:39 +0000

    [TSK#590] creates a CoReSyF tool base class which - parses command line arguments according to a json tool description - prepares inputs, exctracting zip files when needed - cleans up the temporary directory and the end of execution
    Former-commit-id: ff1a0a5fe4e4b97abda50710beb84a9a486d0548 [formerly 103dcd3bba5117ba9337e9ff22e0e7e6cd3905b7] [formerly a2530f4b1c657445886f361863f40fa51707db59 [formerly e240ed7194bc824e7791bb9a5a9b1f68f511cddd]] [formerly ba846331167f1c5957b56c9ada95363eb4682bfe] [formerly 40400ef477d19287f6436f5b1e1f16f12955c9f0 [formerly aae5e6f129e79ae8c0dc360e9f5eccde8acbd10c] [formerly 5b1cce854d446e22bc742b2cd0087d223ae2ba95] [formerly 47fcfdb31bfd64b6f29941e8ffc9ca7afac7ff6f [formerly eac434ba78842379b7f5d657544e95b0a2e9105a] [formerly 8a7a7d7315ccf41361f94b8bf85526677c3b798e [formerly 003ecd2fe8fdb6951870a8fcf3636ddbf4b43d19]] [formerly 1ad04b7b2b974483048448bab438e52e711dd062] [formerly 40400ef477d19287f6436f5b1e1f16f12955c9f0 [formerly aae5e6f129e79ae8c0dc360e9f5eccde8acbd10c] [formerly 5b1cce854d446e22bc742b2cd0087d223ae2ba95] [formerly 40400ef477d19287f6436f5b1e1f16f12955c9f0 [formerly aae5e6f129e79ae8c0dc360e9f5eccde8acbd10c] [formerly 5b1cce854d446e22bc742b2cd0087d223ae2ba95] [formerly 1ad04b7b2b974483048448bab438e52e711dd062 [formerly ba846331167f1c5957b56c9ada95363eb4682bfe [formerly f45a195cd76e70b61e1f42a7389cc07b794fe76d]]]]]]]
    Former-commit-id: 47fcfdb31bfd64b6f29941e8ffc9ca7afac7ff6f [formerly eac434ba78842379b7f5d657544e95b0a2e9105a] [formerly 8a7a7d7315ccf41361f94b8bf85526677c3b798e [formerly 003ecd2fe8fdb6951870a8fcf3636ddbf4b43d19]] [formerly 47fcfdb31bfd64b6f29941e8ffc9ca7afac7ff6f [formerly eac434ba78842379b7f5d657544e95b0a2e9105a] [formerly 8a7a7d7315ccf41361f94b8bf85526677c3b798e [formerly 003ecd2fe8fdb6951870a8fcf3636ddbf4b43d19]] [formerly 1ad04b7b2b974483048448bab438e52e711dd062]]
    Former-commit-id: c5c5d60fb8db553607018f8331134fd74ab214e1
    Former-commit-id: 1ed0b71825d377196cbb75ce28bb0247d9e68ad8 [formerly 0fed46ce3e7b15063ef5fce4346b1d89e3baf4a9]
    Former-commit-id: 8ca07554782a188df2709f6c65aa68bb40f9e31e
    Former-commit-id: db1535e0fbd83ea460de8b913a1b6ddbe166f069 [formerly 3fce8a7af803977c43d35ee9580e4b04973d8288]
    Former-commit-id: 872eb0c6a25142b3c0b8b0b57f15855cd1de6899
    Former-commit-id: e0c47d690222e2aac4464e38e65d3c690d3adcae

---
+++ src/coresyf_tool_base.py
@@ -0,0 +1,182 @@
+import sys
+import os
+
+import shutil
+
+import zipfile
+
+import logging
+
+from cerberus import Validator
+from argparse import ArgumentParser
+
+TMP_DIR = os.path.abspath("tmp")
+
+TOOL_DEF_SCHEMA = {
+    'name': {
+        'type': 'string',
+        'required': True,
+        'empty': False
+    },
+    'description': {
+        'type': 'string'
+    },
+    'arguments': {
+        'type': 'list',
+        'required': True,
+        'schema': {
+            'type': 'dict',
+            'schema': {
+                'identifier': {
+                    'type': 'string',
+                    'required': True,
+                    'empty': False
+                },
+                'name': {
+                    'type': 'string',
+                    'required': True,
+                    'empty': False
+                },
+                'description': {
+                    'type': 'string',
+                    'required': True
+                },
+                'type': {
+                    'type': 'string',
+                    'allowed': ['data', 'parameter', 'output'],
+                    'required': True,
+                    'empty': False
+                },
+                'parameterType': {
+                    'type': 'string'
+                },
+                'dataType': {
+                    'type': 'string'
+                },
+                'default': {
+                    'type': 'string',
+                    'empty': False
+                },
+                'options': {
+                    'type': 'list',
+                    'schema': {
+                        'type': 'string'
+                    }
+                }
+            }
+        }
+    }
+}
+
+TOOL_DEF_SCHEMA_VALIDATOR = Validator(TOOL_DEF_SCHEMA)
+
+
+class InvalidToolDefinitionException(Exception):
+    pass
+
+
+class CoReSyF_Tool(object):
+
+    def __init__(self, tool_definition):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.addHandler(logging.StreamHandler(sys.stdout))
+        self.logger.setLevel(logging.DEBUG)
+        self.inputs = []
+        self.outputs = []
+        self.arg_parser = ArgumentParser()
+        if not TOOL_DEF_SCHEMA_VALIDATOR.validate(tool_definition):
+            raise InvalidToolDefinitionException(
+                TOOL_DEF_SCHEMA_VALIDATOR.errors)
+        self._parse_arguments_(tool_definition)
+
+    def _from_xsd_type_(self, xsd_type):
+        if xsd_type == 'boolean':
+            return bool
+        else:
+            return str
+
+    def _parse_parameter_arg_(self, arg):
+        name = '--' + arg['identifier']
+        _type = self._from_xsd_type_(arg['parameterType'])
+        _help = arg['description']
+        kwargs = {}
+        if 'options' in arg:
+            kwargs['choices'] = arg['options']
+        if 'default' in arg:
+            kwargs['default'] = arg['default']
+        self.arg_parser.add_argument(name, type=_type, help=_help, **kwargs)
+        self.logger.debug('Parsed %s parameter argument.', name)
+
+    def _parse_data_arg_(self, arg):
+        name = arg['identifier']
+        _help = arg['description']
+        kwargs = {}
+        self.arg_parser.add_argument('--' + name, help=_help, **kwargs)
+        self.inputs.append(name)
+        self.logger.debug('Parsed %s data argument.', name)
+
+    def _parse_output_arg_(self, arg):
+        name = arg['identifier']
+        _help = arg['description']
+        kwargs = {}
+        self.arg_parser.add_argument('--' + name, help=_help, **kwargs)
+        self.outputs.append(name)
+        self.logger.debug('Parsed %s output argument.', name)
+
+    def _parse_arguments_(self, tool_definition):
+        for arg in tool_definition['arguments']:
+            if arg['type'] == 'parameter':
+                self._parse_parameter_arg_(arg)
+            elif arg['type'] == 'data':
+                self._parse_data_arg_(arg)
+            elif arg['type'] == 'output':
+                self._parse_output_arg_(arg)
+        if not self.inputs:
+            self.arg_parser.error('Input data argument missing.')
+        if not self.outputs:
+            self.arg_parser.error('Output data argument missing.')
+        self.arguments = self.arg_parser.parse_args()
+
+    def execute(self):
+        self.logger.info('Executing.')
+        self.bindings = vars(self.arguments)
+        self.logger.debug('Bindings: %s', str(self.bindings))
+        self.logger.info('Preparing inputs.')
+        self._prepare_inputs_(self.bindings)
+        self.logger.info('Running.')
+        self.run(self.bindings)
+        self.logger.info('Cleaning temporary data.')
+        self._clean_tmp_()
+
+    def _unzip_file_(self, file_name):
+        extracted_files = None
+        self.logger.debug('Trying to unzip %s.', file_name)
+        if zipfile.is_zipfile(file_name):
+            self.logger.info('Extracting %s .', file_name)
+            archive = zipfile.ZipFile(file_name, 'r')
+            if not archive.infolist():
+                self.arg_parser.error(
+                    "Input zip file '{}' is empty.".format(file_name))
+            archive.extractall(TMP_DIR)
+            archive.close()
+            extracted_files = [os.path.join(TMP_DIR, f)
+                               for f in os.listdir(TMP_DIR)]
+        return extracted_files
+
+    def _prepare_inputs_(self, arguments):
+        for argname in self.inputs:
+            if argname in arguments and arguments[argname]:
+                file_name = arguments[argname]
+                if not os.path.exists(file_name):
+                    self.arg_parser.error("{} does not exists.".format(file_name))
+                else:
+                    extracted_files = self._unzip_file_(file_name)
+                    if extracted_files:
+                        self.bindings[argname] = extracted_files[0]
+
+    def _clean_tmp_(self):
+        if os.path.isdir(TMP_DIR):
+            shutil.rmtree(TMP_DIR)
+
+    def run(self, bindings):
+        pass
