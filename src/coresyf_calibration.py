1299e952173b7454446b9a7e7bde75e5fadce402
Nuno Costa <nuno.costa@deimos.com.pt>
2017-12-20 17:11:32 +0000

    [TSK#590] adds manifest discovery to CoReSyF tool base
    Former-commit-id: af78e1307bf23c0233d0a14496d2f602b66b77aa [formerly dd47784a9fe059d751b226b4c5ebcdd89b7598f8] [formerly 579f4e174b0d41f4f4382e1b807e2a1e381414cb [formerly 2b86b34eba1c102a75e767384b669e2ef12cee87]] [formerly ce4a2510479243ae2cacb42653bac283702b3607] [formerly 3dd565e84b344e62fec2a3e062d228909b68f831 [formerly 17b22809c021f3e728c1c62dc825728e54505a56] [formerly 7d85f908e86f3fdf6cb31027a7b8d3e52ab3c181] [formerly b0db57f6e0af59d81d2556130ba75c12254c0dc5 [formerly e18f5dd425d7f2f1b903ed8ef6d07a752c0d1929] [formerly 38f3c1621adfaee25559b194ee11c324235c4617 [formerly c30959728795265e394a0c2671bdff75ef261690]] [formerly a1af02ff3aaddffa2a2d776466f06f5fbb8b612c] [formerly 3dd565e84b344e62fec2a3e062d228909b68f831 [formerly 17b22809c021f3e728c1c62dc825728e54505a56] [formerly 7d85f908e86f3fdf6cb31027a7b8d3e52ab3c181] [formerly 3dd565e84b344e62fec2a3e062d228909b68f831 [formerly 17b22809c021f3e728c1c62dc825728e54505a56] [formerly 7d85f908e86f3fdf6cb31027a7b8d3e52ab3c181] [formerly a1af02ff3aaddffa2a2d776466f06f5fbb8b612c [formerly ce4a2510479243ae2cacb42653bac283702b3607 [formerly c1fd782cc8a0d57d6dff6df10e674c6b9da4d7c6]]]]]]]
    Former-commit-id: b0db57f6e0af59d81d2556130ba75c12254c0dc5 [formerly e18f5dd425d7f2f1b903ed8ef6d07a752c0d1929] [formerly 38f3c1621adfaee25559b194ee11c324235c4617 [formerly c30959728795265e394a0c2671bdff75ef261690]] [formerly b0db57f6e0af59d81d2556130ba75c12254c0dc5 [formerly e18f5dd425d7f2f1b903ed8ef6d07a752c0d1929] [formerly 38f3c1621adfaee25559b194ee11c324235c4617 [formerly c30959728795265e394a0c2671bdff75ef261690]] [formerly a1af02ff3aaddffa2a2d776466f06f5fbb8b612c]]
    Former-commit-id: f4f370562d390653bdf1efb5acc111323ba875be
    Former-commit-id: a07d5c915135223631f4bf84b7ac7e77edf121d0 [formerly 8c38fdb3ba6bd0e62379a4a8b0c2930d874d351d]
    Former-commit-id: ffabf4b19b8365c0516895d44dba1394b6adef93
    Former-commit-id: 2780be0dde811de6f891aa9c192639a28478016d [formerly b8d801810d1d2148d0c8f2f9b736fa454b09f317]
    Former-commit-id: c00ef902a1f056a6a549ce04d265938789e5fe79
    Former-commit-id: e9f0b68d9a29e02f75088f88581ee0317ecc2ceb

--- src/coresyf_calibration.py
+++ src/coresyf_calibration.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
-import os
-
 from gpt_coresyf_tool import GPTCoReSyFTool
 
-TOOL_DEF_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'coresyf_calibration_tool.json')
-
 if __name__ == '__main__':
-    TOOL = GPTCoReSyFTool(TOOL_DEF_FILE)
-    TOOL.execute()
+    GPTCoReSyFTool(__file__).execute()
