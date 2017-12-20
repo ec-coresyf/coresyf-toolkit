1f9042d62d92dbe838b793346add2af9632586b7
Nuno Costa <nuno.costa@deimos.com.pt>
2017-12-20 16:59:48 +0000

    [TSK#590] puts gpt operation as a value in the manifest file
    Former-commit-id: 6797b9ebaecd3c2025894582c3e8cc148f1c34ed [formerly 7901db444c820d5d4283a8025ed22c3e932f7b3c] [formerly 9c5fae60d77419edc351bdd47cbb1a67a43f58a8 [formerly 67ed667a4e5d94a0f7619081a67811eedaf336ea]] [formerly 308bc2dda048249aa2639fd6d9f9fd87356446a5] [formerly d4c15f7a8ba9220f908191d7c9a4a2fc08f95e70 [formerly faf4cd18ce3eafcc7cd7d37f435eeaa5fff4bd06] [formerly 8358178339fd33f95b28fb06b7a14ba5431e2b60] [formerly 485e294226278fe2b623be774bdcde23fb3d4e58 [formerly 7db38b8bdc34fd015c76bc691415e450228dc4e7] [formerly 724622ba8be26117994dfd19a6843feb477489e1 [formerly c4305e3491d39ee126f19bdb8dfab950dc11b329]] [formerly 6fa54c39ab4c1c3861c8016349dee354a0bc7523] [formerly d4c15f7a8ba9220f908191d7c9a4a2fc08f95e70 [formerly faf4cd18ce3eafcc7cd7d37f435eeaa5fff4bd06] [formerly 8358178339fd33f95b28fb06b7a14ba5431e2b60] [formerly d4c15f7a8ba9220f908191d7c9a4a2fc08f95e70 [formerly faf4cd18ce3eafcc7cd7d37f435eeaa5fff4bd06] [formerly 8358178339fd33f95b28fb06b7a14ba5431e2b60] [formerly 6fa54c39ab4c1c3861c8016349dee354a0bc7523 [formerly 308bc2dda048249aa2639fd6d9f9fd87356446a5 [formerly 82d1b2f4a719c87400ee21ed4e69b6948c5b82ae]]]]]]]
    Former-commit-id: 485e294226278fe2b623be774bdcde23fb3d4e58 [formerly 7db38b8bdc34fd015c76bc691415e450228dc4e7] [formerly 724622ba8be26117994dfd19a6843feb477489e1 [formerly c4305e3491d39ee126f19bdb8dfab950dc11b329]] [formerly 485e294226278fe2b623be774bdcde23fb3d4e58 [formerly 7db38b8bdc34fd015c76bc691415e450228dc4e7] [formerly 724622ba8be26117994dfd19a6843feb477489e1 [formerly c4305e3491d39ee126f19bdb8dfab950dc11b329]] [formerly 6fa54c39ab4c1c3861c8016349dee354a0bc7523]]
    Former-commit-id: caafa896787d341b3ec72fe974939091cd687c42
    Former-commit-id: 404e7d101e53c8323172ecd9c581d788222aff5b [formerly 4c21fd7aee2e5a143319f3dcec17546e12960027]
    Former-commit-id: 724810309f30482b4df9b912a492c500fa310fe1
    Former-commit-id: 8f54418e7be24b8ce45b53052312fefd83a7c936 [formerly 842572c6dc87322945bda51298761e86e9c38722]
    Former-commit-id: 3fbf2c4bdd6d078b4a8b846b88dfad8078c2b1e4
    Former-commit-id: 364e89f18c6cb28a7f2754d2ccb5939590d1e9f7

--- src/coresyf_calibration.py
+++ src/coresyf_calibration.py
@@ -6,5 +6,5 @@ from gpt_coresyf_tool import GPTCoReSyFTool
 TOOL_DEF_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'coresyf_calibration_tool.json')
 
 if __name__ == '__main__':
-    TOOL = GPTCoReSyFTool(TOOL_DEF_FILE, 'Calibration')
+    TOOL = GPTCoReSyFTool(TOOL_DEF_FILE)
     TOOL.execute()
