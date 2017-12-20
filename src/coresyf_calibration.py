871bc222bfaece75750f3a1064e8b026ebc8fea6
Nuno Costa <nuno.costa@deimos.com.pt>
2017-12-20 16:53:11 +0000

    [TSK590] moves calibration docstring to README
    Former-commit-id: 32fc9871e665fa4d51745cb42d4ee8e81fa2f447 [formerly 7ef344d5bbab511cc7b8fe5221b3dcb71e012933] [formerly 15f14714c28b281c1e81137dd22f165f725258d8 [formerly f192d4df2342337df4841be4dab2110c82c81779]] [formerly b0d2cc0ce90b9648cadf0320598058000799b015] [formerly 72e178bbfd2094616d61d069ab83b1acbf966b0d [formerly 069d16b7805211e83a1302513a5258e9e9c4c4dc] [formerly aa2c4fa33524b19359c58b0c84e381b90f96d02b] [formerly 9db712c5079ee22ffe5ca24d80b1f875dbca0717 [formerly 921d5d04d34cef06c504e5893ab814200808e7c6] [formerly 198b9176d03b1708a4d3689bec8ac76e58bf2c3c [formerly 7c23399f5aabb3555059400a00c3b720041893b6]] [formerly f11a406c406f664099e2502e9ff3ae762c4de939] [formerly 72e178bbfd2094616d61d069ab83b1acbf966b0d [formerly 069d16b7805211e83a1302513a5258e9e9c4c4dc] [formerly aa2c4fa33524b19359c58b0c84e381b90f96d02b] [formerly 72e178bbfd2094616d61d069ab83b1acbf966b0d [formerly 069d16b7805211e83a1302513a5258e9e9c4c4dc] [formerly aa2c4fa33524b19359c58b0c84e381b90f96d02b] [formerly f11a406c406f664099e2502e9ff3ae762c4de939 [formerly b0d2cc0ce90b9648cadf0320598058000799b015 [formerly 552164d207466b7717ad5434b645b7ef0844479c]]]]]]]
    Former-commit-id: 9db712c5079ee22ffe5ca24d80b1f875dbca0717 [formerly 921d5d04d34cef06c504e5893ab814200808e7c6] [formerly 198b9176d03b1708a4d3689bec8ac76e58bf2c3c [formerly 7c23399f5aabb3555059400a00c3b720041893b6]] [formerly 9db712c5079ee22ffe5ca24d80b1f875dbca0717 [formerly 921d5d04d34cef06c504e5893ab814200808e7c6] [formerly 198b9176d03b1708a4d3689bec8ac76e58bf2c3c [formerly 7c23399f5aabb3555059400a00c3b720041893b6]] [formerly f11a406c406f664099e2502e9ff3ae762c4de939]]
    Former-commit-id: 4252c750e047d56099e55785e6c9ce60d3b11453
    Former-commit-id: be504e0175c1c528c0b900d7056952fec72e93bc [formerly fbe8e9fce09985bf86211f65f5190562cca74724]
    Former-commit-id: da5063d2339a184ba732c46ec9f34796499e31aa
    Former-commit-id: e00b50df978ffa2a7929d2a66b17cae8bb0ca8ea [formerly dd97fd5ca168bb7bf1e76d697a404af3863b925b]
    Former-commit-id: b5b4bcc75de6b1fe79989017e375fdf18995d392
    Former-commit-id: 0014ea5ec37d4dc7cbc01c87814a9997204f469a

--- src/coresyf_calibration.py
+++ src/coresyf_calibration.py
@@ -1,32 +1,5 @@
 #!/usr/bin/env python
-'''
-@summary: 
-This module runs the following Co-ReSyF tool:
- - CALIBRATION
- It uses the command line based Graph Processing Tool (GPT) to perform radiometric
-callibration of mission products. The input must be the whole product (use either
- the 'manifest.safe' or the whole product in a zip file as input).
-
-@example:
-
-Example 1 - Calibrate a Sentinel product S1A:
-./coresyf_calibration.py --Ssource S1A_EW_GRDM_1SDH_20171001T060648_20171001T060753_018615_01F62E_EE7F.zip 
-                         --Ttarget myfile
-
-@attention: 
-    @todo
-    - Add a product file to be used as example in 'examples' directory
-    - Develop test script
-
-@version: v.1.0
-
-@change:
-1.0
-- First release of the tool. 
-'''
 import os
-
-import json
 
 from gpt_coresyf_tool import GPTCoReSyFTool
 
