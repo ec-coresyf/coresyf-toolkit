a9bc4cbf94530b41728b5f6dcc2ea735b5a96e6a
Nuno Costa <nuno.costa@deimos.com.pt>
2017-12-20 19:10:31 +0000

    [TSK#590] packages coresyftools module
    Former-commit-id: 369801abe3688b3b9e03a954916257918d31b7cc

--- src/coresyf_calibration.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-from gpt_coresyf_tool import GPTCoReSyFTool
-
-if __name__ == '__main__':
-    GPTCoReSyFTool(__file__).execute()
---
+++ src/coresyftools/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='CoReSyF tools',
+      version='0.1',
+      description='base classes for CoReSyF tool',
+      packages=['coresyftools'])
