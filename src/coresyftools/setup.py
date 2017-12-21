0523b7c9f56f12903af4810d33c4d3f040921301
Nuno Costa <nuno.costa@deimos.com.pt>
2017-12-21 16:36:28 +0000

    [TSK#590] updates coresyftools version
    Former-commit-id: 1f0352d1cb00adbd9d16dcb6b1c5efc7ed95a6c6

--- src/coresyftools/setup.py
+++ src/coresyftools/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='CoReSyF tools',
-      version='0.1',
+      version='0.2',
       description='base classes for CoReSyF tool',
       packages=['coresyftools'],
       install_requires=[
