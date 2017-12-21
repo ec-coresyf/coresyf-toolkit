a2763edb81347cfc8d64553e6ef5e074bb0b20f1
Nuno Costa <nuno.costa@deimos.com.pt>
2017-12-21 12:35:50 +0000

    [TSK/590] adds cerberus requirement to coresyftools package
    Former-commit-id: be91710a15cb8d93355321f04afcbb8efa3c134d

--- src/coresyftools/setup.py
+++ src/coresyftools/setup.py
@@ -3,4 +3,7 @@ from setuptools import setup
 setup(name='CoReSyF tools',
       version='0.1',
       description='base classes for CoReSyF tool',
-      packages=['coresyftools'])
+      packages=['coresyftools'],
+      install_requires=[
+            'cerberus'
+      ])
