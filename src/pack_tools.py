9d066856128e3f42d11994419ea1b8621844c880
Nuno Costa <nuno.costa@deimos.com.pt>
2017-12-21 16:37:01 +0000

    [TSK#590] adds gpt_graph.xml to packaging in pack_toos
    Former-commit-id: 29374d86aabf11b4bd226f393d54c8b0e83a900a [formerly d4f6028c100bfe53fdce8fd331d6b69f0595c3bb] [formerly 506c41b49b4295ee0534cc52f384b4891ad0ec77 [formerly f79aca83a410e51eb8339572d9709d2e891838a1]] [formerly d17302a18c40d37ae9ef6c4f3dc53851ecd22258] [formerly d41db4a79bc6d80c685c900373035f2155ade1bd [formerly 8a15859ac5d6a896600c48ab368d798d166456cc] [formerly 2e79b4afa028a5dd64605bb59c49f505d85aa81b] [formerly 76b3eae001a45b2e43eec121a9149a49e38d511a [formerly b7f5da33794d50854b39c473c575cb5a4b2897c9] [formerly 0555ef477ab755d557818e3b13d8ac599578a741 [formerly 490112e5d69c78b2b7634c181cc4508e66f9e42c]] [formerly 7f5c6f28417bf6bbfe271ae8cb27b504972157c1] [formerly d41db4a79bc6d80c685c900373035f2155ade1bd [formerly 8a15859ac5d6a896600c48ab368d798d166456cc] [formerly 2e79b4afa028a5dd64605bb59c49f505d85aa81b] [formerly d41db4a79bc6d80c685c900373035f2155ade1bd [formerly 8a15859ac5d6a896600c48ab368d798d166456cc] [formerly 2e79b4afa028a5dd64605bb59c49f505d85aa81b] [formerly 7f5c6f28417bf6bbfe271ae8cb27b504972157c1 [formerly d17302a18c40d37ae9ef6c4f3dc53851ecd22258 [formerly beca62f9e4779fec67d310ead0c01e12132db3a7]]]]]]]
    Former-commit-id: 76b3eae001a45b2e43eec121a9149a49e38d511a [formerly b7f5da33794d50854b39c473c575cb5a4b2897c9] [formerly 0555ef477ab755d557818e3b13d8ac599578a741 [formerly 490112e5d69c78b2b7634c181cc4508e66f9e42c]] [formerly 76b3eae001a45b2e43eec121a9149a49e38d511a [formerly b7f5da33794d50854b39c473c575cb5a4b2897c9] [formerly 0555ef477ab755d557818e3b13d8ac599578a741 [formerly 490112e5d69c78b2b7634c181cc4508e66f9e42c]] [formerly 7f5c6f28417bf6bbfe271ae8cb27b504972157c1]]
    Former-commit-id: 1d05c6a9eb8fa179744ef9d5231a0aab418baecd
    Former-commit-id: a85b9418828cf8f02265704794233b91122b5fb5 [formerly 8da8d22bbae3a28f42effa362936cfd26fccf0d8]
    Former-commit-id: a1afef05fb6c45805daca30f6dc333c4f90ffc0d
    Former-commit-id: bfc0f9a0fc7e1627caf8456631f94a3516741b75 [formerly 2b2d430a56eac7413ad8b53ab80f1949f63a81f4]
    Former-commit-id: 0b36da6c763064d6e42097aee5b7625f344bb751
    Former-commit-id: 4353bd6582bdb17857c967f78384e9e5d883f4ab

--- src/pack_tools.py
+++ src/pack_tools.py
@@ -6,11 +6,13 @@ from glob import glob
 from shutil import copy, make_archive
 
 
-def prepare_tool(output_dir, name, run_file, manifest_file):
+def prepare_tool(output_dir, name, run_file, manifest_file, gpt_graph_file=None):
     tool_dir = join(output_dir, name)
     mkdir(tool_dir)
     copy(run_file, join(tool_dir, 'run'))
     copy(manifest_file, join(tool_dir, 'manifest.json'))
+    if gpt_graph_file:
+        copy(gpt_graph_file, join(tool_dir, 'gpt_graph.xml'))
     make_archive(tool_dir, 'zip', tool_dir)
 
 
@@ -20,7 +22,10 @@ def prepare_toolkit(toolkit_dir, output_dir):
                  for file in listdir(toolkit_dir) if file.endswith('.json')]
     for manifest_file in manifests:
         tool_name = splitext(basename(manifest_file))[0]
-        prepare_tool(output_dir, tool_name, run_file, manifest_file)
+        gpt_graph_file = join(toolkit_dir, tool_name + '.gpt_graph.xml')
+        if not os.path.exists(gpt_graph_file):
+            gpt_graph_file = None
+        prepare_tool(output_dir, tool_name, run_file, manifest_file, gpt_graph_file)
 
 
 if __name__ == '__main__':
