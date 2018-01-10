5466efc2ab58abd051c535b7d8ea8fea809b0947
Nuno Costa <nuno.costa@deimos.com.pt>
2018-01-10 18:12:12 +0000

    [TSK#590] improves tool_tester tests
    Former-commit-id: 172049393a32cb5cc5923c14c94fa6c14334c3b0

--- src/coresyftools/coresyftools/tests/test_tester.py
+++ src/coresyftools/coresyftools/tests/test_tester.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from tool_tester import ToolTester
+from tool_tester import ToolTester, NonZeroReturnCode, NonEmptyStderr, NoOutputFile, EmptyOutputFile
 
 
 class TestTester(TestCase):
@@ -9,8 +9,12 @@ class TestTester(TestCase):
         tester = ToolTester('tests/dummy_tool')
         tester.test()
         self.assertEqual(len(tester.errors), 4)
+        self.assertIsInstance(tester.errors[0], NonZeroReturnCode)
         self.assertEqual(tester.errors[0].returncode, 1)
         self.assertIsNotNone(tester.errors[0].stderr)
+        self.assertIsInstance(tester.errors[1], NonEmptyStderr)
         self.assertEqual(tester.errors[1].message, 'stderrmsg')
+        self.assertIsInstance(tester.errors[2], NoOutputFile)
         self.assertEqual(tester.errors[2].file, 'void_output')
+        self.assertIsInstance(tester.errors[3], EmptyOutputFile)
         self.assertEqual(tester.errors[3].file, 'empty_output')
