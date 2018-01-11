30c2b0cea2daef9ccf7ca0316b4118d977a062fc
Nuno Costa <nuno.costa@deimos.com.pt>
2018-01-11 13:46:56 +0000

    [TSK#590] tests manifest validation
    Former-commit-id: ac40af91607f48066c6ed7036dc0a81dcbf35024

--- src/coresyftools/coresyftools/tests/test_coresyf_tool_base.py
+++ src/coresyftools/coresyftools/tests/test_coresyf_tool_base.py
@@ -1,6 +1,94 @@
 from unittest import TestCase
 
-from coresyf_tool_base import CoReSyFArgParser
+from coresyf_tool_base import CoReSyFArgParser, validate_manifest, MalformedManifestException, MissingInputArgument, MissingOutputArgument
+
+
+class TestValidateManifest(TestCase):
+
+    def test_valid(self):
+        base_manifest = {
+            "name": "name",
+            "description": "description",
+            "arguments": [{
+                "identifier": "input",
+                "name": "input",
+                "description": "input description",
+                "type": "data",
+                "required": True
+            }, {
+                "identifier": "output",
+                "name": "output",
+                "description": "Sets the output file name to <filepath>",
+                "type": "output"
+            }]
+        }
+        validate_manifest(base_manifest)
+
+    def test_missing_key(self):
+        manifest = {
+            "arguments": [{
+                "identifier": "input",
+                "name": "input",
+                "description": "input description",
+                "type": "data",
+                "required": True
+            }, {
+                "identifier": "output",
+                "name": "output",
+                "description": "Sets the output file name to <filepath>",
+                "type": "output"
+            }]
+        }
+        self.assertRaises(MalformedManifestException,
+                          lambda: validate_manifest(manifest))
+
+    def test_invalid_key(self):
+        manifest = {
+            "name": "name",
+            "description_______": "description",
+            "arguments": [{
+                "identifier": "input",
+                "name": "input",
+                "description": "input description",
+                "type": "data",
+                "required": True
+            }, {
+                "identifier": "output",
+                "name": "output",
+                "description": "Sets the output file name to <filepath>",
+                "type": "output"
+            }]
+        }
+        self.assertRaises(MalformedManifestException,
+                          lambda: validate_manifest(manifest))
+
+    def test_no_input(self):
+        manifest = {
+            "name": "name",
+            "description": "description",
+            "arguments": [{
+                "identifier": "output",
+                "name": "output",
+                "description": "Sets the output file name to <filepath>",
+                "type": "output"
+            }]
+        }
+        self.assertRaises(MissingInputArgument,
+                          lambda: validate_manifest(manifest))
+
+    def test_no_output(self):
+        manifest = {
+            "name": "name",
+            "description": "description",
+            "arguments": [{
+                "identifier": "input",
+                "name": "input",
+                "description": "Sets the output file name to <filepath>",
+                "type": "data"
+            }]
+        }
+        self.assertRaises(MissingOutputArgument,
+                          lambda: validate_manifest(manifest))
 
 
 class TestCoReSyFArgParser(TestCase):
@@ -168,7 +256,4 @@ class TestCoReSyFArgParser(TestCase):
 
         def parse():
             self.parse_with_arg(arg, command.split())
-        self.assertRaises(SyntaxError, parse)
-
-
-
+        self.assertRaises(SystemExit, parse)
