5e6a20791fab60dbe9746e3cb0d58a3e19cd6315
Tiago Mendes <tiago.mendes@deimos.com.pt>
2018-01-12 17:23:27 +0000

    [TSK#590] Improved shipper
    Former-commit-id: 2558ea15bd376dc1f87c853918f3b32c6200668a [formerly de7d5e8576eba4c4caf7dd7e32855729aafc590b] [formerly f23893b28f488afd6de01cfb3e3ff253fbb8cd18 [formerly f09bb155bc5a0f97ee7cf8a928776ecf7eea1da3]] [formerly 0ac81b98e2eee910978d1d85454413860f01b3d1] [formerly f5be135c8b747ff828cc90639c25887ebf58e7c2 [formerly 01a5d22e90d78629b66afc1790676264b6e18273] [formerly 2f33d8b4d6975c9d95d030928a39f6084ba2dc9f] [formerly fdab419e96f18e3caff222396c7fe5cdba81121f [formerly 87378ff2b2bf201452c6e83a630341c78db1462d] [formerly 2b10809156aefe6d3c71e386e55de482c95ef469 [formerly 79b8490fbce9d75f0042b968adbf935675c27ed8]] [formerly 07ffe00aa2c94dfd53d70a326d68e0f508235b20] [formerly f5be135c8b747ff828cc90639c25887ebf58e7c2 [formerly 01a5d22e90d78629b66afc1790676264b6e18273] [formerly 2f33d8b4d6975c9d95d030928a39f6084ba2dc9f] [formerly f5be135c8b747ff828cc90639c25887ebf58e7c2 [formerly 01a5d22e90d78629b66afc1790676264b6e18273] [formerly 2f33d8b4d6975c9d95d030928a39f6084ba2dc9f] [formerly 07ffe00aa2c94dfd53d70a326d68e0f508235b20 [formerly 0ac81b98e2eee910978d1d85454413860f01b3d1 [formerly 56c59c356b675120ff7ab4a9bf46dea3758d8f4e]]]]]]]
    Former-commit-id: fdab419e96f18e3caff222396c7fe5cdba81121f [formerly 87378ff2b2bf201452c6e83a630341c78db1462d] [formerly 2b10809156aefe6d3c71e386e55de482c95ef469 [formerly 79b8490fbce9d75f0042b968adbf935675c27ed8]] [formerly fdab419e96f18e3caff222396c7fe5cdba81121f [formerly 87378ff2b2bf201452c6e83a630341c78db1462d] [formerly 2b10809156aefe6d3c71e386e55de482c95ef469 [formerly 79b8490fbce9d75f0042b968adbf935675c27ed8]] [formerly 07ffe00aa2c94dfd53d70a326d68e0f508235b20]]
    Former-commit-id: 6dd732bd2ce62d15f3d8b2a655c3e42888a27dfe
    Former-commit-id: 73068e4988ac589724750f7ef0c069f75c768d43 [formerly 24dbf3befa1d643364c0ae5943d3b672172c8923]
    Former-commit-id: c799141c31ffb482fa437a3fedaf3a3d937aa504
    Former-commit-id: 5389cac6cbbb0c97e1719e24f59b2dc171c81481 [formerly 209d921da750b647dfd702f1f8b935ad9818f601]
    Former-commit-id: 51857a4065a47e50c54d61c1241e8278e21bec4f
    Former-commit-id: 524b53ac9d5f5afbe93c1f2ae85c3d0a857dd81e

--- src/wings_shipper/wings_shipper.py
+++ src/wings_shipper/wings_shipper.py
@@ -13,9 +13,40 @@ from wings_api.data import ManageData
 @click.option('--wpass', help='Password for username')
 def deploy_application(ipath, wurl, wdomain, wuser, wpass):
     """The wrapper function to deploy the wings app"""
-    click.echo('Deploying application to wings. Reading from %s' % ipath)
-    shipper = WingsShipper(wurl, wdomain, wuser, wpass, ipath)
-    shipper.ship_wings_tool()
+    click.echo(click.style(
+        '[INFO] Reading manifest file: %s' % ipath, fg='blue'))
+
+    try:
+        shipper = WingsShipper(wurl, wdomain, wuser, wpass, ipath)
+        click.echo(click.style(
+            '[INFO] %s: %s' % (shipper.manifest['name'],
+                            shipper.manifest['description']),
+            fg='blue'))
+        click.echo(click.style(
+            '[INFO] Type: %s' % shipper.manifest['type'], fg='blue'))
+        click.echo(click.style(
+            '[INFO] %i inputs' % len(shipper.manifest['inputs']), fg='blue'))
+        click.echo(click.style(
+            '[INFO] %i parameters' % len(shipper.manifest['parameters']), fg='blue'))
+        click.echo(click.style(
+            '[INFO] %i outputs' % len(shipper.manifest['outputs']), fg='blue'))
+        click.echo(click.style(
+            '[INFO] RUNNING SHIPPER...', fg='blue', bold=True))
+    except Exception as e:
+        click.echo(click.style(
+            '[ERROR] Application encountered an error initializing: %s' % str(e),
+            fg='red', bold=True))
+        return
+    try:
+        shipper.ship_wings_tool()
+        click.echo(click.style(
+            '[SUCCESS] Application shipped!', fg='green', bold=True
+        ))
+    except Exception as e:
+        click.echo(click.style(
+            '[ERROR] Application encountered an error initializing: %s' % str(e),
+            fg='red', bold=True))
+        return
 
 
 class WingsShipper(object):
@@ -38,15 +69,16 @@ class WingsShipper(object):
         self.component_manager.login(wings_pass)
         self.manifest_path = manifest_path
         self.manifest = {}
+        self.read_manifest()
 
     def ship_wings_tool(self):
         """Ships the tool to wings, adding it as a new component"""
-        self.read_manifest()
         self.create_component_type()
         self.create_component()
         inputs_list = self.parse_data_argument(self.manifest['inputs'])
         params_list = self.parse_parameter_argument(self.manifest['parameters'])
         outputs_list = self.parse_data_argument(self.manifest['outputs'])
+        self.add_component_properties(inputs_list, params_list, outputs_list)
 
     def read_manifest(self):
         """Reads the manifest file and loads it into a dict"""
@@ -55,18 +87,13 @@ class WingsShipper(object):
     def create_component_type(self):
         """Attempts to retrieve a component type. If it doesn't exist in wings,
         it creates a new one"""
-        self.component_manager.add_component_type(self.get_component_type_id())
+        self.component_manager.add_component_type(self.manifest['type'])
 
     def create_component(self):
         """Attempts to retrieve a component. If it doesn't exist in wings,
         it creates a new one"""
-        self.component_manager.add_component(self.get_component_type_id(),
+        self.component_manager.add_component(self.manifest['type'],
                                              self.get_component_id())
-
-    def get_component_type_id(self):
-        """Returns the component type, which consists of appending
-        the keyword 'type' at the end of the component"""
-        return '{}Type'.format(self.get_component_id())
 
     def get_component_id(self):
         """Reads the manifest file and returns the component ID.
@@ -86,7 +113,7 @@ class WingsShipper(object):
                 'dimensionality': _data['dimensionality']
             })
         return data_list
-    
+
     def parse_parameter_argument(self, raw_parameters):
         parameter_list = []
         for _parameter in raw_parameters:
@@ -102,6 +129,10 @@ class WingsShipper(object):
         """Attempts to retrieve a data type. If it doesn't exist in wings,
         it creates a new one"""
         self.data_manager.new_data_type(data_type)
+
+    def add_component_properties(self, inputs, parameters, outputs):
+        self.component_manager.add_component_parameters(
+            self.get_component_id(), inputs, parameters, outputs, '')
 
 
 if __name__ == '__main__':
