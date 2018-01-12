a5a708dff11830b4bbcdec31c47eda1fae49b1e5
Tiago Mendes <tiago.mendes@deimos.com.pt>
2018-01-12 10:11:06 +0000

    [TSK#590] Added sample folder and updated manifest
    Former-commit-id: 47d396323e1eb008b11c2cdc04e5cf7fb72ebfde [formerly fc5ef4cf5f4d70a76ae31623fc713742a33a01a3] [formerly ee0e602c09e84cdf3ed1006b0214cefe6bbbc65f [formerly 949e65bed58b3e7bf58e0e34e2b6fc0d21168b82]] [formerly 581e70aec84fa2b6169b54724cf379492cae7243] [formerly 7801cf694185763ccf498bafdfa74380acfa1706 [formerly 8580f809969560dbb2f80238c58940f3edf7fba0] [formerly e90ed2efbc99aea4455050175f789b4205b0ad00] [formerly e5a4ed49967ed98785795c761d40e2b66514b080 [formerly 5066f48ad9b4cd1690ce6e054cd98b3de7011a23] [formerly 6ffa9f3213386190cfac257c41301394d6d7533a [formerly 16903fe668aa1b5ccd90b41fbcff6689ceef4b3b]] [formerly 0344f7da3b104efe5d5b71e24b0bb895ad309465] [formerly 7801cf694185763ccf498bafdfa74380acfa1706 [formerly 8580f809969560dbb2f80238c58940f3edf7fba0] [formerly e90ed2efbc99aea4455050175f789b4205b0ad00] [formerly 7801cf694185763ccf498bafdfa74380acfa1706 [formerly 8580f809969560dbb2f80238c58940f3edf7fba0] [formerly e90ed2efbc99aea4455050175f789b4205b0ad00] [formerly 0344f7da3b104efe5d5b71e24b0bb895ad309465 [formerly 581e70aec84fa2b6169b54724cf379492cae7243 [formerly ae389e9c3874aa022fff71d754cf0eaaaa8784c1]]]]]]]
    Former-commit-id: e5a4ed49967ed98785795c761d40e2b66514b080 [formerly 5066f48ad9b4cd1690ce6e054cd98b3de7011a23] [formerly 6ffa9f3213386190cfac257c41301394d6d7533a [formerly 16903fe668aa1b5ccd90b41fbcff6689ceef4b3b]] [formerly e5a4ed49967ed98785795c761d40e2b66514b080 [formerly 5066f48ad9b4cd1690ce6e054cd98b3de7011a23] [formerly 6ffa9f3213386190cfac257c41301394d6d7533a [formerly 16903fe668aa1b5ccd90b41fbcff6689ceef4b3b]] [formerly 0344f7da3b104efe5d5b71e24b0bb895ad309465]]
    Former-commit-id: 1cab3bc753147d3cf479ea124cfb34b65ffea051
    Former-commit-id: fd49952c55190b31425fbdcb952da23f54976039 [formerly 9fa6ecc85f1fda18bfc81a5137abfb5a2b17a25d]
    Former-commit-id: 2e3d26d010a39228cba83b37d4b6a26eb349b460
    Former-commit-id: 6b5d90d142ad11d20882618defdef16e822b1cba [formerly 29e68a67f68194d1d2e60fef134be3496afb3917]
    Former-commit-id: c046761cda7feb37c112c7816840ea1d05ca1bff
    Former-commit-id: c4431df1783f8ed7adc4c668648d11456aeec9bb

--- src/wings_shipper/deploy_wings_application.py
+++ src/wings_shipper/deploy_wings_application.py
@@ -1,10 +1,14 @@
+"""A command line utility to deploy a wings application"""
 import click
 from wings_api.components import ManageComponents
+
 
 @click.command()
 @click.option('--ipath', help='Path to zip file containing application manifest')
 def deploy_application(ipath):
+    """The wrapper function to deploy the wings app"""
     click.echo('Deploying application to wings. Reading from %s' % ipath)
+
 
 if __name__ == '__main__':
     deploy_application()
