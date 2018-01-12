6f3f528f121e33a777f06927eec216bac93a7a8f
Tiago Mendes <tiago.mendes@deimos.com.pt>
2018-01-12 15:26:31 +0000

    [TSK#590] Implemented mehtod to add a component type
    Former-commit-id: 6425f2483dd2ead09cac4a8c3cf31097d71c2302 [formerly 026db8b5baa624b444b328a95614822dae68abf2] [formerly 7487515375158d411951d63da3fc2fd1b6bf736c [formerly d9e7036da98be2c6b954d435e608926e00ca66d4]] [formerly 9b5626cf22104183b00dfca0000ae6e0e3328b74] [formerly 79c1de602b0ec4fb2d434e8d4bb9318beda9fc8d [formerly 3b3c940e01fcfb1df1aa9c5b341e42e9c912c2c8] [formerly 9a3bc5a251b48165124d15d6a5f818121ce54dbd] [formerly cf5c957e3678352a08171fffb4c8c69c31905764 [formerly 0766bcf99ab0a88bff35ff0890cbce3ed373de2e] [formerly af21e0624f61e186617459302838bfa39c75ad82 [formerly 1f10a1e4671e78f950f2099d2b7813fb1587a462]] [formerly 54dbfc0fd3fd2a235119c687219d05c2412ef0af] [formerly 79c1de602b0ec4fb2d434e8d4bb9318beda9fc8d [formerly 3b3c940e01fcfb1df1aa9c5b341e42e9c912c2c8] [formerly 9a3bc5a251b48165124d15d6a5f818121ce54dbd] [formerly 79c1de602b0ec4fb2d434e8d4bb9318beda9fc8d [formerly 3b3c940e01fcfb1df1aa9c5b341e42e9c912c2c8] [formerly 9a3bc5a251b48165124d15d6a5f818121ce54dbd] [formerly 54dbfc0fd3fd2a235119c687219d05c2412ef0af [formerly 9b5626cf22104183b00dfca0000ae6e0e3328b74 [formerly 803f4271490fa4694d85e8ead3316c1924a4d26a]]]]]]]
    Former-commit-id: cf5c957e3678352a08171fffb4c8c69c31905764 [formerly 0766bcf99ab0a88bff35ff0890cbce3ed373de2e] [formerly af21e0624f61e186617459302838bfa39c75ad82 [formerly 1f10a1e4671e78f950f2099d2b7813fb1587a462]] [formerly cf5c957e3678352a08171fffb4c8c69c31905764 [formerly 0766bcf99ab0a88bff35ff0890cbce3ed373de2e] [formerly af21e0624f61e186617459302838bfa39c75ad82 [formerly 1f10a1e4671e78f950f2099d2b7813fb1587a462]] [formerly 54dbfc0fd3fd2a235119c687219d05c2412ef0af]]
    Former-commit-id: fc7fd2bae9309b1a71e7dfb82a328abf2ab17855
    Former-commit-id: 67eede54a9f210f0059e52ac925a72753f4cc205 [formerly f11bb4896643d163a6f7a6d56d4b9049a7beac13]
    Former-commit-id: 4cf9987d1d74fa432bab37a6a28a5959ef68eb21
    Former-commit-id: b19bb7ecf9218a22eca84cd3b7c9fb39929d2d24 [formerly c8d656814cca72e0139ca4889588d8ef06efeed7]
    Former-commit-id: bda923e15d5b359b00ebed4c6185b6d61596837b
    Former-commit-id: 30f81e45fbe12fc1743abae7e9cf1e89023fbac9

--- src/wings_shipper/wings_shipper.py
+++ src/wings_shipper/wings_shipper.py
@@ -8,49 +8,67 @@ from wings_api.data import ManageData
 @click.command()
 @click.option('--ipath', help='Path to the manifest file')
 @click.option('--wurl', help='WINGS Instance URL')
+@click.option('--wdomain', help='Wings domain to use')
 @click.option('--wuser', help='Username for wings domain')
 @click.option('--wpass', help='Password for username')
-def deploy_application(ipath, wurl, wuser, wpass):
+def deploy_application(ipath, wurl, wdomain, wuser, wpass):
     """The wrapper function to deploy the wings app"""
     click.echo('Deploying application to wings. Reading from %s' % ipath)
-    shipper = WingsShipper(wurl, wuser, wpass, ipath)
-    print(shipper.manifest)
+    shipper = WingsShipper(wurl, wdomain, wuser, wpass, ipath)
+    shipper.parse_manifest()
 
 
 class WingsShipper(object):
     """A class to read manifest files and create appropriate wings components"""
-    def __init__(self, wings_url, wings_user, wings_pass, manifest_path):
+
+    def __init__(self, wings_url, wings_domain, wings_user,
+                 wings_pass, manifest_path):
         """Initiates shipper class and instantiates manage data and components.
 
         :param str wings_url: url for the wings instance to use
-        :param str wings_user: username of user where component 
+        :param str wings_domain: the wings domain to use        
+        :param str wings_user: username of user where component
         shall be added
         :param str wings_pass: password to login the user
         """
-        self.data_manager = ManageData(wings_url, wings_user, wings_pass)
-        self.component_manager = ManageComponents(wings_url, wings_user, wings_pass)
+        self.data_manager = ManageData(wings_url, wings_user, wings_domain)
+        self.component_manager = ManageComponents(
+            wings_url, wings_user, wings_domain)
+        self.data_manager.login(wings_pass)
+        self.component_manager.login(wings_pass)
         self.manifest_path = manifest_path
+        self.manifest = {}
         self.read_manifest()
 
     def read_manifest(self):
         """Reads the manifest file and loads it into a dict"""
         self.manifest = json.load(open(self.manifest_path))
-    
+
     def parse_manifest(self):
         """Converts the manifest file into valid parameters to be uploaded to
         wings."""
+        # A component's ID should not have spaces in the name.
+        component_id = self.manifest['name'].replace(' ', '')
+        component_type_id = '{}{}'.format(component_id, 'Type')
+
+        self.create_component_type(component_type_id)
+
+    def create_component_type(self, component_type_id):
+        """Attempts to retrieve a component type. If it doesn't exist in wings,
+        it creates a new one"""
+        self.data_manager.new_data_type('MyType')
+        self.component_manager.add_component_type(component_type_id)
+
+    def create_component(self, component_type_id):
+        """Attempts to retrieve a component. If it doesn't exist in wings,
+        it creates a new one"""
         pass
 
-    def get_or_create_data_type(self, data_type):
+    def create_data_type(self, data_type):
         """Attempts to retrieve a data type. If it doesn't exist in wings,
         it creates a new one"""
         pass
-    
-    def get_or_create_component_type(self, component_type):
-        """Attempts to retrieve a component type. If it doesn't exist in wings,
-        it creates a new one"""
-        pass
-    
+
     def ship_wings_tool(self):
         """Ships the tool to wings, adding it as a new component"""
         pass
