fda8520e5d0818d7468af5098fdafbc26ef2dd5b
Tiago Mendes <tgmendes@gmail.com>
2018-02-21 17:10:13 +0000

    feat#629: improve code and read wings parameters from config file

--- src/wings_shipper/wings_shipper.py
+++ src/wings_shipper/wings_shipper.py
@@ -1,4 +1,5 @@
 """A command line utility to deploy a wings application"""
+from os.path import exists
 import click
 import json
 from wings_api.components import ManageComponents
@@ -13,41 +14,64 @@ from wings_api.data import ManageData
 @click.option('--wpass', help='Password for username')
 def deploy_application(ipath, wurl, wdomain, wuser, wpass):
     """The wrapper function to deploy the wings app"""
-    click.echo(click.style(
-        '[INFO] Reading manifest file: %s' % ipath, fg='blue'))
-
+    try:
+        validate_inputs(ipath, wurl, wdomain, wuser, wpass)
+    except Exception as e:
+        click_log(str(e), 'ERROR')
+        return
+    
+    if not is_wings_parameters_defined(wurl, wdomain, wuser, wpass):
+        wings_parameters = json.load(open('wings_config.json'))
+        wurl = wings_parameters['url']
+        wdomain = wings_parameters['domain']
+        wuser = wings_parameters['user']
+        wpass = wings_parameters['password']
+    
     try:
         shipper = WingsShipper(wurl, wdomain, wuser, wpass, ipath)
-        click.echo(click.style(
-            '[INFO] %s: %s' % (shipper.manifest['name'],
-                            shipper.manifest['description']),
-            fg='blue'))
-        click.echo(click.style(
-            '[INFO] Type: %s' % shipper.manifest['type'], fg='blue'))
-        click.echo(click.style(
-            '[INFO] %i inputs' % len(shipper.manifest['inputs']), fg='blue'))
-        click.echo(click.style(
-            '[INFO] %i parameters' % len(shipper.manifest['parameters']), fg='blue'))
-        click.echo(click.style(
-            '[INFO] %i outputs' % len(shipper.manifest['outputs']), fg='blue'))
-        click.echo(click.style(
-            '[INFO] RUNNING SHIPPER...', fg='blue', bold=True))
+        click_log('{}: {}'.format(
+            shipper.manifest['name'], shipper.manifest['description']), 'INFO')
+        click_log('Type: {}'.format(shipper.manifest['type']), 'INFO')
+        click_log('{} inputs'.format(len(shipper.manifest['inputs'])), 'INFO')
+        click_log('{} parameters'.format(len(shipper.manifest['parameters'])), 'INFO')
+        click_log('{} outputs'.format(len(shipper.manifest['outputs'])), 'INFO')
+        click_log('RUNNING SHIPPER...', 'INFO')
+
     except Exception as e:
-        click.echo(click.style(
-            '[ERROR] Application encountered an error initializing: %s' % str(e),
-            fg='red', bold=True))
+        click_log(str(e), 'ERROR')
         return
     try:
         shipper.ship_wings_tool()
-        click.echo(click.style(
-            '[SUCCESS] Application shipped!', fg='green', bold=True
-        ))
+        click_log('Application shipped!', 'SUCCESS')
+        
     except Exception as e:
-        click.echo(click.style(
-            '[ERROR] Application encountered an error initializing: %s' % str(e),
-            fg='red', bold=True))
+        click_log(str(e), 'ERROR')
         return
 
+def validate_inputs(ipath, wurl, wdomain, wuser, wpass):
+    if not exists(ipath):
+        raise ValueError('Input path does not exist')
+    if (wurl or wdomain or wuser or wpass) and \
+        not (wurl and wdomain and wuser and wpass):
+        raise AttributeError('All wings parameters must be defined.')
+    if not is_wings_parameters_defined(wurl, wdomain, wuser, wpass) and \
+        not exists('wings_config.json'):
+        raise AttributeError('Wings parameters undefined and config file does not exist')
+
+def is_wings_parameters_defined(wurl, wdomain, wuser, wpass):
+    return (wurl and wdomain and wuser and wpass)
+    
+def click_log(message, error_type):
+    color = 'blue'
+    bold = False
+    if error_type == 'ERROR':
+        color = 'red'
+        bold = True
+    elif error_type == 'SUCCESS':
+        color = 'green'
+        bold = True
+
+    click.echo(click.style('[%s] %s' % (error_type, message), fg=color, bold=bold))
 
 class WingsShipper(object):
     """A class to read manifest files and create appropriate wings components"""
@@ -76,7 +100,8 @@ class WingsShipper(object):
         self.create_component_type()
         self.create_component()
         inputs_list = self.parse_data_argument(self.manifest['inputs'])
-        params_list = self.parse_parameter_argument(self.manifest['parameters'])
+        params_list = self.parse_parameter_argument(
+            self.manifest['parameters'])
         outputs_list = self.parse_data_argument(self.manifest['outputs'])
         self.add_component_properties(inputs_list, params_list, outputs_list)
 
@@ -110,9 +135,18 @@ class WingsShipper(object):
                 'role': _data['identifier'],
                 'type': _data['type'],
                 'prefix': '--{}'.format(_data['identifier']),
-                'dimensionality': _data['dimensionality']
+                'dimensionality': self.get_dimensionality_value(_data)
             })
         return data_list
+
+    @staticmethod
+    def get_dimensionality_value(data_argument):
+        """Check if the data argument has a collection parameter set to true.
+        This means that the dimensionality will be 1 (we don't use dimensionalities
+        greater than that)"""
+        if data_argument.get('collection'):
+            return 1
+        return 0
 
     def parse_parameter_argument(self, raw_parameters):
         parameter_list = []
