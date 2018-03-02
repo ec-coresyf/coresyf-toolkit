da35ba05a6b9f1cfaed41914d74aec3acdbec411
rmcapote <ricardocapote@hotmail.com>
2018-03-02 16:01:59 +0000

    Task#540 Corrected and completed adaptive thresholding tool.

---
+++ src/coresyf_adaptive_thresholding/examples.sh
@@ -0,0 +1,3 @@
+#PASSING EXECUTION
+#Example of a passing execution
+./run --Ssource ../../examples/SNAP_GPT_Image/ASA_WSM_1PXPDE20021117_104431_000000672011_00180_03741_0009.N1_Cal.tif --Ttarget myoutput.tif
