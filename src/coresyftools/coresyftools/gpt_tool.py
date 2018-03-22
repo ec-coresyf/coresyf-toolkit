9d89faf3b3ed117a34ea6545d11f3c5ca158ab33
Nuno Costa <nuno.costa@deimos.com.pt>
2018-03-22 14:25:27 +0000

    task/771: adds tif extension to source input defined with GeoTIFF type

--- src/coresyftools/coresyftools/gpt_tool.py
+++ src/coresyftools/coresyftools/gpt_tool.py
@@ -36,6 +36,7 @@ class GPTCoReSyFTool(CoReSyFTool):
     DEFAULT_FORMAT = 'GeoTIFF-BigTIFF'
     DEFAULT_EXT = 'tif'
     DEFAULT_GPT_GRAPH_FILE_NAME = 'gpt_graph.xml'
+    GEOTIFF_TYPE = 'GeoTIFF'
 
     def _graph_file_path(self, operation_dict):
         graph_file_name = self.DEFAULT_GPT_GRAPH_FILE_NAME
@@ -58,7 +59,7 @@ class GPTCoReSyFTool(CoReSyFTool):
             graph_file = self._graph_file_path(operation)
             if not os.path.exists(graph_file):
                 raise GPTGraphFileNotFound(graph_file)
-
+    
     def run(self, bindings):
         if len(self.arg_parser.inputs) > 1:
             raise TooManyInputArgumentsException()
@@ -75,15 +76,22 @@ class GPTCoReSyFTool(CoReSyFTool):
         if 'parameters' in self.operation:
             bindings.update(self.operation['parameters'])
         source = bindings.pop(self.arg_parser.inputs[0])
-        source_with_ext = self._add_snap_file_extension(source)
+        source_with_ext = self._add_snap_file_extension(source) if self._has_geotiff_source() else source
         target = bindings.pop(self.arg_parser.outputs[0])
         self._call_gpt(operator, source_with_ext, target, bindings)
         # Remove source file extension (case it was added)
-        self._remove_snap_file_extension(source)
+        if self._has_geotiff_source():
+            self._remove_snap_file_extension(source)
         # This is needed because SNAP automatically adds file extensions, but
         # output files can not have a name different from the specified in
         # the command line.
         self._remove_snap_file_extension(target)
+
+    def _has_geotiff_source(self):
+        return self._get_source_type() == self.GEOTIFF_TYPE 
+    
+    def _get_source_type(self):
+         return self.manifest['inputs'][0]['type']
 
     def _build_gpt_shell_command(self, operator, source, target, options):
         source = os.path.abspath(source)
