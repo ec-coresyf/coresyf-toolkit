dcbfefeb958170cd8d4ff6743bf8340a6f2aa228
rirr <ricardo.rocha@deimos.com.pt>
2018-04-06 15:15:27 +0100

    feature#789: correct example files.

--- src/coresyf_object_discrimination/examples.sh
+++ src/coresyf_object_discrimination/examples.sh
@@ -1,4 +1,4 @@
-ASA_WSM_1PXPDE20021117_104431_000000672011_00180_03741_0009.N1.zip="http://example.org"
+myoutput.tif="http://example.org"
 #PASSING EXECUTION
 #Example of a passing execution
 ./run --Ssource myoutput.tif --Ttarget myoutput_object.tif
