2734c9cde7329f1a531ae00edb7b5dd76daacced
rirr <ricardo.rocha@deimos.com.pt>
2018-04-06 15:15:27 +0100

    feature#789: correct example files.

--- src/coresyf_adaptive_thresholding/examples.sh
+++ src/coresyf_adaptive_thresholding/examples.sh
@@ -1,4 +1,4 @@
-ASA_WSM_1PNUPA20050206_102659_000000672034_00280_15364_1195.N1.zip="http://example.org"
+calibrated.tif="http://example.org"
 #PASSING EXECUTION
 #Example of a passing execution
 ./run --Ssource calibrated.tif --Ttarget myoutput.tif
