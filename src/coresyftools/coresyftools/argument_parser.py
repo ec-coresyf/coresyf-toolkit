405aa4345755fe3bca880f459b0c37cdc61986fb
rmcapote <ricardocapote@hotmail.com>
2018-04-09 16:02:01 +0100

    Task#798 Correction on parse_arguments method.

--- src/coresyftools/coresyftools/argument_parser.py
+++ src/coresyftools/coresyftools/argument_parser.py
@@ -27,7 +27,7 @@ class CoReSyFArgumentParser():
         self.bindings = vars(self.arguments)
 
         self.bindings = dict([(k, v) for k, v in self.bindings.items()
-                        if v is not None])
+                        if v and v is not None])
         for opt in self.options:
             self.bindings[opt] = self.bindings[opt] if opt in self.bindings \
                                                     else False
