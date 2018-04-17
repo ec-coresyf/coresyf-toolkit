61dea8bc66180860883c29f0f6e95865d37bac75
rmcapote <ricardocapote@hotmail.com>
2018-04-17 16:12:29 +0100

    Task#837 Added coresyf_calibration_ecgg_speckle tool.

---
+++ src/coresyf_calibration_ecgg_speckle/examples.sh
@@ -0,0 +1,4 @@
+ASA_WSM_1PXPDE20021117_104431_000000672011_00180_03741_0009.N1.gz="http://example.org"
+#PASSING EXECUTION
+#Example of a passing execution
+./run --Ssource ASA_WSM_1PXPDE20021117_104431_000000672011_00180_03741_0009.N1.gz --Ttarget myoutput.tif
