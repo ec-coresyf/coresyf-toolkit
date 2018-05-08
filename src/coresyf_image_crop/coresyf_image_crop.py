32a2fdd0356bd7bd252818e03f4aeaa488315ce6
rmcapote <ricardocapote@hotmail.com>
2018-05-08 19:14:35 +0100

    Task#804 Added a function to read zipped shapefile. Corrected get_shapefile_polygon_extent to receive a datasource ogr object as argument.

--- src/coresyf_image_crop/coresyf_image_crop.py
+++ src/coresyf_image_crop/coresyf_image_crop.py
@@ -1,14 +1,44 @@
 #!/usr/bin/python2
 from osgeo import ogr
+import zipfile
+import sys
+import os
 
 from coresyftools.tool import CoReSyFTool
 from sridentify import Sridentify
 
 
-def get_shapefile_polygon_extent(shapefile_path):
+def read_zip_shapefile(filepath, temp_path="temp_input"):
+    '''
+    It opens a zip file containing a shapefile and returns an handle to the
+    OGRDataSource (a GDAL OGR object with the shapefile data).
+    '''
+    zip_file = zipfile.ZipFile(filepath, 'r')
+    if not zip_file.infolist():
+        sys.exit("Input Zip file with shapefile '%s' is empty!" % filepath)
+    # Extract zip contents
+    zip_file.extractall(temp_path)
+    zip_file.close()
+    # Get main file of the shapefile
+    input_list = [os.path.join(temp_path, x) for x in os.listdir(temp_path)
+                  if x.endswith(".shp")]
+    if not input_list:
+        sys.exit("Shapefile not found in '%s'!" % filepath)
+    shapefile_shp = input_list[0]
+    try:
+        driver = ogr.GetDriverByName('ESRI Shapefile')
+        data_source = driver.Open(shapefile_shp, 0)  # 0 means read-only.
+        if not data_source:
+            raise IOError()
+    except IOError:
+        sys.exit("Error. Unable to open shapefile '%s'!" % filepath)
+    except Exception:
+        sys.exit("Unexpected error when opening shapefile '%s'!" % filepath)
+    return data_source
+
+
+def get_shapefile_polygon_extent(data_source):
     '''Retrieves the extent of the grid represented as OGR:Polygon.'''
-    driver = ogr.GetDriverByName("ESRI Shapefile")
-    data_source = driver.Open(shapefile_path, 0)
     layer = data_source.GetLayer()
     extent = layer.GetExtent()
 
@@ -28,6 +58,7 @@ class CoresyfImageCrop(CoReSyFTool):
 
     def run(self, bindings):
         pass
-        # get_shapefile_crs(shapefile_path) - retrieves the ESPG code of the shapefile CRS.
-        # get_shapefile_polygon_extent(shapefile_path) - retrieves the extent of the grid represented as OGR:Polygon.
+        # read_zip_shapefile(shapefile_zip_path)
+        # get_shapefile_crs(ogr_datasource) - retrieves the ESPG code of the shapefile CRS.
+        # get_shapefile_polygon_extent(ogr_datasource) - retrieves the extent of the grid represented as OGR:Polygon.
         # crop_raster(raster_path, output_path, extent_polygon, output_crs) - applies the crop to the input image by executing gdal_warp in command line.
