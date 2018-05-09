b1f398bd4b547a72266ac9819c217f47040f91fe
rirr <ricardo.rocha@deimos.com.pt>
2018-05-09 15:02:09 +0100

    task#862: create test.

--- src/coresyf_image_crop/tests/test_image_crop.py
+++ src/coresyf_image_crop/tests/test_image_crop.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
-from ..coresyf_image_crop import get_shapefile_polygon_extent, read_zip_shapefile
-
+import shutil
+from ..coresyf_image_crop import get_shapefile_polygon_extent, \
+    get_shapefile_crs, read_zip_shapefile
 
 class TestImageCrop(TestCase):
 
@@ -11,7 +12,13 @@ class TestImageCrop(TestCase):
         pass
 
     def test_get_shapefile_polygon_extent(self):
-        data_source = read_zip_shapefile('/home/rccc/Downloads/Crop_Tool_DESIGN/grid_EPSG_3763.zip')
+        data_source = read_zip_shapefile('grid_EPSG_3763.shx.zip')
         get_shapefile_polygon_extent(data_source)
         print ("Ok")
-        pass
+        shutil.rmtree('temp_input')
+
+    def test_get_shapefile_crs(self):
+        data_source = read_zip_shapefile('grid_EPSG_3763.shx.zip')
+        espg_code = get_shapefile_crs(data_source)
+        self.assertEqual(espg_code, 3763)
+        shutil.rmtree('temp_input')
