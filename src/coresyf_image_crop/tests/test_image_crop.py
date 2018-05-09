9541f3996f70b2532cdb8af89ba497be81158c35
rirr <ricardo.rocha@deimos.com.pt>
2018-05-09 15:26:15 +0100

    task#862: organize tests.

--- src/coresyf_image_crop/tests/test_image_crop.py
+++ src/coresyf_image_crop/tests/test_image_crop.py
@@ -1,24 +1,29 @@
 from unittest import TestCase
+import os
 import shutil
 from ..coresyf_image_crop import get_shapefile_polygon_extent, \
     get_shapefile_crs, read_zip_shapefile
 
+TEMP_PATH = "temp_path"
+
+
 class TestImageCrop(TestCase):
 
     def setUp(self):
-        pass
+        self.data_source = read_zip_shapefile('test_data/grid_EPSG_3763.zip', 
+                                              TEMP_PATH)
 
     def tearDown(self):
-        pass
+        if os.path.isdir(TEMP_PATH):
+            shutil.rmtree(TEMP_PATH)
 
     def test_get_shapefile_polygon_extent(self):
         data_source = read_zip_shapefile('test_data/grid_EPSG_3763.shx.zip')
         get_shapefile_polygon_extent(data_source)
         print ("Ok")
-        shutil.rmtree('temp_input')
 
     def test_get_shapefile_crs(self):
         data_source = read_zip_shapefile('test_data/grid_EPSG_3763.shx.zip')
         espg_code = get_shapefile_crs(data_source)
         self.assertEqual(espg_code, 3763)
-        shutil.rmtree('temp_input')
+        
