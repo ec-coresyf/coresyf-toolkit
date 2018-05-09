4a711714ebb14f2f098ca6314aa3dae0176c04fd
rirr <ricardo.rocha@deimos.com.pt>
2018-05-09 15:03:07 +0100

    task#826: change test_data directory.

--- src/coresyf_image_crop/tests/test_image_crop.py
+++ src/coresyf_image_crop/tests/test_image_crop.py
@@ -12,13 +12,13 @@ class TestImageCrop(TestCase):
         pass
 
     def test_get_shapefile_polygon_extent(self):
-        data_source = read_zip_shapefile('grid_EPSG_3763.shx.zip')
+        data_source = read_zip_shapefile('test_data/grid_EPSG_3763.shx.zip')
         get_shapefile_polygon_extent(data_source)
         print ("Ok")
         shutil.rmtree('temp_input')
 
     def test_get_shapefile_crs(self):
-        data_source = read_zip_shapefile('grid_EPSG_3763.shx.zip')
+        data_source = read_zip_shapefile('test_data/grid_EPSG_3763.shx.zip')
         espg_code = get_shapefile_crs(data_source)
         self.assertEqual(espg_code, 3763)
         shutil.rmtree('temp_input')
