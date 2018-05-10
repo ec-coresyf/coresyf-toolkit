5b5c5c4cc1971a5d29c64e991d23efd880db8a5e
rirr <ricardo.rocha@deimos.com.pt>
2018-05-10 17:16:25 +0100

    task#864: add test.

--- src/coresyf_image_crop/tests/test_image_crop.py
+++ src/coresyf_image_crop/tests/test_image_crop.py
@@ -1,8 +1,9 @@
 from unittest import TestCase
 import os
 import shutil
+from osgeo import ogr
 from ..coresyf_image_crop import get_shapefile_polygon_extent, \
-    get_shapefile_crs, read_zip_shapefile
+    get_shapefile_crs, read_zip_shapefile, crop_raster
 
 TEMP_PATH = "temp_path"
 
@@ -33,3 +34,13 @@ class TestImageCrop(TestCase):
     def test_get_shapefile_crs(self):
         epsg_code = get_shapefile_crs(self.data_source)
         self.assertEqual(epsg_code, self.epsg_code)
+
+    def test_crop_raster(self):
+        test_image = 'test_data/Aveiro_20170131T183449_20170131T183514_004096_007148_CDB9_Sigma0_VV_processed.tif'
+        output_image = os.path.join(TEMP_PATH, 'output')
+        polygon_ext = ogr.CreateGeometryFromWkt(self.extent_polygon_wkt)
+        polygon_ext = polygon_ext.Buffer(2000, 0)
+        crop_raster(test_image, output_image, polygon_ext, self.epsg_code)
+        self.assertNotEqual(os.path.getsize(output_image), 0)
+        self.assertLess(os.path.getsize(output_image),
+                        os.path.getsize(test_image))
