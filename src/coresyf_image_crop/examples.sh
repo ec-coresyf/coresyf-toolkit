da51d15a6f9f09a2d61eb5f198077d4b4660ef48
rirr <ricardo.rocha@deimos.com.pt>
2018-05-14 14:32:10 +0100

    task#864: add examples file.

---
+++ src/coresyf_image_crop/examples.sh
@@ -0,0 +1,4 @@
+./tests/test_data/Aveiro_resampled.tif.zip="http://example.org"
+#PASSING EXECUTION
+#Example of a passing execution
+./run --Ssource ./tests/test_data/Aveiro_resampled.tif.zip --Sgrid ./tests/test_data/grid_EPSG_3763.zip --Ttarget myoutput.tif
