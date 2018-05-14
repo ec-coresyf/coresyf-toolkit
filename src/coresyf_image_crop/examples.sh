25fb6e358f2bdfc22286140b6f0b83ec41cb532e
rirr <ricardo.rocha@deimos.com.pt>
2018-05-14 16:20:45 +0100

    task#864: change tool to not unzip grid files.

--- src/coresyf_image_crop/examples.sh
+++ src/coresyf_image_crop/examples.sh
@@ -1,4 +1,4 @@
-./tests/test_data/Aveiro_resampled.tif.zip="http://example.org"
+Aveiro_resampled.tif="http://example.org"
 #PASSING EXECUTION
 #Example of a passing execution
-./run --Ssource ./tests/test_data/Aveiro_resampled.tif.zip --Sgrid ./tests/test_data/grid_EPSG_3763.zip --Ttarget myoutput.tif
+./run --Ssource Aveiro_resampled.tif --Sgrid grid_EPSG_3763.zip --Ttarget myoutput.tif
