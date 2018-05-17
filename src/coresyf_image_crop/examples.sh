0af7662f3b29959da3e23c268a21a968bdb3580a
rirr <ricardo.rocha@deimos.com.pt>
2018-05-17 18:55:55 +0100

    task#865: add default for buffer parameter.

--- src/coresyf_image_crop/examples.sh
+++ src/coresyf_image_crop/examples.sh
@@ -1,4 +1,4 @@
 Aveiro_resampled.tif="http://example.org"
 #PASSING EXECUTION
 #Example of a passing execution
-./run --Ssource Aveiro_resampled.tif --Sgrid grid_EPSG_3763.zip --Ttarget myoutput.tif
+./run --Ssource Aveiro_resampled.tif --Pbuffer 5 --Sgrid grid_EPSG_3763.zip --Ttarget myoutput.tif
