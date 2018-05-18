fcf67156ffc2dc3aa8a7fec911ba482c0b596dd2
rccc <ricardo.capote@deimos.com.pt>
2018-05-18 19:52:04 +0100

    NoTask Corrected Image Crop examples.sh.

--- src/coresyf_image_crop/examples.sh
+++ src/coresyf_image_crop/examples.sh
@@ -1,4 +1,4 @@
 Aveiro_resampled.tif="http://example.org"
 #PASSING EXECUTION
 #Example of a passing execution
-./run --Ssource Aveiro_resampled.tif --Pbuffer 5 --Sgrid grid_EPSG_3763.zip --Ttarget myoutput.tif
+./run --Ssource tests/test_data/Aveiro_resampled.tif --Pbuffer 5 --Sgrid tests/test_data/grid_EPSG_3763 --Ttarget myoutput.tif
