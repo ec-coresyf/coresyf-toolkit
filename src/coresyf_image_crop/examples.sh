d479e779c2138a45a414c015aa5951b35808c3b8
rccc <ricardo.capote@deimos.com.pt>
2018-05-25 15:19:09 +0100

    Task#876 Corrected examples file.

--- src/coresyf_image_crop/examples.sh
+++ src/coresyf_image_crop/examples.sh
@@ -2,4 +2,6 @@ Aveiro_resampled.tif="http://example.org"
 #PASSING EXECUTION
 #Example of a passing execution
 ./run --Ssource tests/test_data/Aveiro_resampled.tif --Pbuffer 5 --Sgrid tests/test_data/grid_EPSG_3763 --Ttarget myoutput1.tif
+#PASSING EXECUTION2
+#Example of a passing execution. Here the buffer is provided in meters.
 ./run --Ssource tests/test_data/Aveiro_resampled.tif --Pbuffer 1000 --PbufferUnits meters --Sgrid tests/test_data/grid_EPSG_3763 --Ttarget myoutput2.tif
