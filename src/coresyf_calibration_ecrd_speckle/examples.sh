abec8aa6d48726b2c9edb8515ab8841ff3db0421
rccc <ricardo.capote@deimos.com.pt>
2018-06-04 11:40:11 +0100

    Task#726 Corrected test file and updated execution example.

--- src/coresyf_calibration_ecrd_speckle/examples.sh
+++ src/coresyf_calibration_ecrd_speckle/examples.sh
@@ -1,4 +1,4 @@
 ASA_WSM_1PXPDE20021117_104431_000000672011_00180_03741_0009.N1="http://example.org"
 #PASSING EXECUTION
 #Example of a passing execution
-./run --Ssource subset_of_S1B_IW_GRDH_1SDV_20170131T183449_20170131T183514_004096_007148_CDB9.data.zip --Ppolarisations VV --Pfilter 'Lee Sigma' --PauxFile 'Product Auxiliary File' --Ttarget myoutput.tif
+./run --Ssource subset_0_of_S1B_IW_GRDH_1SDV_20170131T183449_20170131T183514_004096_007148_CDB9.data.zip --Ppolarisations VV --Pfilter 'Lee Sigma' --PauxFile 'Product Auxiliary File' --Ttarget myoutput.tif
