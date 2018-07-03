e4c2554c2c488d4cd89fb592356749cb33df2baf
Julius Schröder <julius-schroeder@posteo.de>
2018-07-03 11:32:30 +0100

    switsch from old local repo to new

---
+++ src/coresyf_data_cube_creation/coresyfDataCubeCreation.py
@@ -0,0 +1,240 @@
+# coding: utf-8
+# ! python2
+"""This module collects function to creates 3D data cube of grid data.
+
+The data cube is a 3D data structure grid with following axes:
+
+    1. time
+    2. columns
+    3. rows
+
+The 3D data structure is a NetCDF file. Each slice of this axes is array of data for one day.
+The grid structure is defined by columns and rows with additional dimensions.
+"""
+
+__author__ = "Julius Schroeder"
+__version__ = "0.0.1"
+__copyright__ = "Copyright 2018, MaREI Centre for Marine and Renewable Energy"
+
+# import python modules
+import glob
+import itertools
+import logging
+import numpy as np
+import os
+import sys
+
+from collections import namedtuple
+
+# import third party modules
+from netCDF4 import Dataset
+
+from coresyftools.tool import CoReSyFTool
+
+def get_inputs(folder, pattern="*.nc"):
+    """This searching for input files in folder matching pattern parameter
+    returns list of path.
+
+    Parameters
+    ----------
+
+    folder: str
+        Path of folder to search in.
+
+    pattern: regex string
+        Pattern for file of interests as regex string.
+
+    Returns
+    -------
+    inputs: iterator
+        List of file in the folder matching the pattern.
+
+    Example
+    -------
+    > search_for_files("data/")
+    """
+
+    search_path = os.path.join(folder, pattern)
+    inputs = glob.iglob(search_path)
+    # TODO: add results logging
+
+    try:
+        first = inputs.next()
+    except StopIteration:
+        msg = "No files found in {}! Search pattern are {}."
+        raise IOError(msg.format(folder, search_path))
+    else:
+        inputs = itertools.chain([first], inputs)
+        return inputs
+
+
+
+
+def extract_slice(file_path, variables=None):
+    """Extract data from dataset by given variables.
+
+    This gets a netCDF file with spatial data as input file. As default all available variables will be extract from dataset, but optional the function get also a list of data variables to extract.
+
+    Parameters
+    ----------
+
+    file_path: str
+        Path to netCDF file.
+
+    variables: list
+        List of variables to extract from dataset. Default all variables will by extract.
+
+    Returns
+    -------
+    data: dict
+        Dictionary of extracted data. Variables names used as keys. Data are numpy.ma.MaskedArray instances. Additional dimensions key is used to save latitude and longitude dimensions.
+
+    Example
+    -------
+    > # Extract SST data from GHRSST netCDF4 file.
+    > extract_var = ["mask", "analysed_sst", "analysis_error"]
+    > extract_data("data/ghrsst.nc", extract_var)
+
+    """
+
+
+    data = {
+        'dimensions': {
+            'lat': None,
+            'lon': None,
+        },
+    }
+
+    try:
+        dataset = Dataset(file_path, "r", format="NETCDF4")
+    except IOError as e:
+        raise e("Can not open {}.".format(file_path))
+
+    lat = dataset.variables["lat"][:]
+    lon = dataset.variables["lon"][:]
+
+    data['dimensions']['lat'] = lat
+    data['dimensions']['lon'] = lon
+
+    # filter by variable
+    if not variables:
+        keys = dataset.variables.keys()
+        variables = [var for var in keys if dataset.variables[var].ndim == 2] # only 2D variables
+
+    for var in variables:
+        try:
+            set = dataset.variables[var][:, :]
+        except KeyError:
+            msg = 'Variable {} is not in dataset.'
+            logging.debug(KeyError(msg.format(var)))
+        except ValueError:
+            msg = 'Variable {} number of dimension < 2.'
+            logging.debug(ValueError(msg.format(var)))
+        except IndexError as e:
+            raise e
+        else:
+            data[var] = set
+
+    dataset.close()
+
+    return data
+
+
+# TODO: write documentation
+def create_stack_file(data, ds_path, dimensions_names=("time", "lat", "lon")):
+
+    # TODO: add IOError messages in log
+    try:
+        dataset = Dataset(ds_path, 'w', format="NETCDF4")
+    except IOError:
+        raise
+    else:
+        with dataset:
+
+            # create dimensions
+            for dim in data['dimensions']:
+                if data['dimensions'][dim] is None:
+                    dataset.createDimension(dim, None)  # add as unlimited dimension
+                else:
+                    dataset.createDimension(dim, len(data['dimensions'][dim]))
+
+            # create infinite time dimmension
+            dataset.createDimension("time", None)
+
+            # create variables
+            for var in data.keys():
+                if var == 'dimensions':
+                    for dim in data['dimensions']:
+                        dim_var = dataset.createVariable(dim, data['dimensions'][dim].dtype, (dim,))
+                        dim_var[:] = data['dimensions'][dim]
+                else:
+                    dataset.createVariable(var, data[var].dtype, dimensions_names, zlib=True)
+
+def write_slice(slice, ds_path):
+
+    try:
+        dataset = Dataset(ds_path, 'a', format="NETCDF4")
+    except IOError as e:
+        raise
+    else:
+        with dataset:
+            # create stack by adding layers
+            slice_time = len(dataset.dimensions["time"])
+
+            for var in slice.keys():
+                if var != 'dimensions':
+                    var_data = dataset.variables[var]
+                    var_data[slice_time, :, :] = slice[var]
+
+def stacking(inputs, variables, output):
+    """This go over alle inputs, extract data and write results to file."""
+
+    for input__ in inputs:
+        logging.info('Extracting data from {}'.format(set))
+        slice = extract_slice(input__, variables=variables)
+
+        # run masking
+        # TODO: fix masking to zero and one
+        # data = masked_by_flags(data, mask_var="mask", flags=[2,5,9,13])
+
+        if os.path.exists(output) is False:
+            logging.info('Create {} file.'.format(output))
+
+            try:
+                create_stack_file(slice, output)
+            except EnvironmentError as e:
+                logging.error("Can't create {}".format(output))
+                logging.debug((os.strerror(e.errno)))
+                raise e
+
+        try:
+            write_slice(slice, output)
+        except EnvironmentError as e:
+            os.remove(output)
+
+            logging.error("Can't write to {}".format(output))
+            logging.debug((os.strerror(e.errno)))
+            raise e
+
+
+class CoReSyFDataCubeCreation(CoReSyFTool):
+
+    def run(self, bindings):
+
+        try:
+            inputs = get_inputs(input_dir, pattern="*.nc")
+        except IOError as e:
+            logging.error("No inputs found.".format(output))
+            logging.debug((os.strerror(e.errno)))
+            sys.exit()
+
+        # parse Parameters
+        input_folder = bindings['Ssource']
+        variables = tuple(bindings['var'].split(','))
+        output = bindings['Ttarget']
+
+        try:
+            stacking(inputs, variables, output)
+        except Exception as e:
+            raise e
+            sys.exit()
--- src/coresyf_data_cube_creation/tests/test_data_cube_creation.py
+++ src/coresyf_data_cube_creation/tests/test_data_cube_creation.py
@@ -4,38 +4,210 @@
 
 import numpy as np
 import unittest
-from os import path
+import tempfile
+import os
 
+from netCDF4 import Dataset
 
-from coresyf_data_cube_creation.data_cube import search_files
-from coresyf_data_cube_creation.data_cube import extract_data
+from coresyf_data_cube_creation.coresyfDataCubeCreation import get_inputs
+from coresyf_data_cube_creation.coresyfDataCubeCreation import extract_slice
+from coresyf_data_cube_creation.coresyfDataCubeCreation import create_stack_file
+from coresyf_data_cube_creation.coresyfDataCubeCreation import write_slice
+from coresyf_data_cube_creation.coresyfDataCubeCreation import stacking
 
 
 # Change if test data is changed
-TEST_DATA = path.join('coresyf_data_cube_creation',
+TEST_FOLDER = os.path.join('coresyf_data_cube_creation',
+        'test_data',
+        'GHRSST',)
+
+TEST_DATA = os.path.join('coresyf_data_cube_creation',
         'test_data',
         'GHRSST',
         '20170101090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc')
+
 VARIABLES = ['mask', 'analysed_sst', 'analysis_error']
 
+
+# helper functions
+
+def get_data_dict():
+    """Return data dict by using extract_data()"""
+    file_path = os.path.abspath(TEST_DATA)
+    return extract_slice(file_path, variables=VARIABLES)
+
+def get_dest_path(f_path='output.nc'):
+    tempdir = tempfile.mkdtemp()
+    return os.path.join(tempdir, f_path)
+
+def create_netcdf_file(data, dest_path):
+    create_stack_file(data, dest_path)
+
+class TestGetInputs(unittest.TestCase):
+    def setUp(self):
+        self.inputdir = tempfile.mkdtemp()
+
+    def test_input_folder_empty(self):
+        empty = self.inputdir
+        with self.assertRaises(IOError):
+            inputs = get_inputs(empty, pattern="*.nc")
+
+    def test_inputs_found(self):
+        pass
 
 
 class TestExtractData(unittest.TestCase):
     def setUp(self):
-        self.file_path = TEST_DATA
-        pass
+        self.file_path = os.path.abspath(TEST_DATA)
 
-    def test_extract_data_has_dimensions(self):
-        """This text data extraction by variable."""
-        file_path = path.abspath(self.file_path)
-        no_bbox = None
+    def test_create_dimensions(self):
+        """This tests if data dict has dimensions."""
 
-        data = extract_data(file_path, no_bbox)
-        self.assertIsNotNone(data['dimensions']['lat'])
-        self.assertIsNotNone(data['dimensions']['lon'])
-        self.assertTrue(data['dimensions']['lat'].size > 0)
-        self.assertTrue(data['dimensions']['lon'].size > 0)
+        data = extract_slice(self.file_path)
+        self.assertIsNotNone(data['dimensions']['lat'], "Latitude dimension is undefined!")
+        self.assertIsNotNone(data['dimensions']['lon'], "Longitude dimension is undefined")
+        self.assertTrue(data['dimensions']['lat'].size > 0, "Latitude dimension is empty!")
+        self.assertTrue(data['dimensions']['lon'].size > 0, "Longitude dimension is empty!")
 
-    
+    def test_extract_variable(self):
+        """This tests if data dict has variables."""
+        data = extract_slice(self.file_path, variables=['analysed_sst'])
+        self.assertIsNotNone(data['analysed_sst'], "Variable is not in data dict!")
+        self.assertTrue(data['analysed_sst'].size > 0, "Variable is empty!")
+
+    def test_extract_variables_list(self):
+        """This tests if data dict has variables."""
+        data = extract_slice(self.file_path, variables=['analysed_sst','analysis_error'])
+        self.assertIsNotNone(data['analysed_sst'], "Variable is not in data dict!")
+        self.assertTrue(data['analysed_sst'].size > 0, "Variable is empty!")
+
+    def test_variables_are_2D(self):
+        """This tests if data dict has variables."""
+        data = extract_slice(self.file_path)
+        self.assertTrue(data['analysed_sst'].ndim == 2, "Variable is not 2D!")
+
+    def test_variable_not_in_dataset(self):
+        """Test print error message if variable is not in dataset."""
+        data = extract_slice(self.file_path, variables=['not_in_dataset'])
+        with self.assertRaises(KeyError):
+            data['not_in_dataset']
+
+    def test_skipping_not_in_dataset_variables(self):
+        """Test print error message if variable is not in dataset."""
+        data = extract_slice(self.file_path, variables=['not_in_dataset', 'analysed_sst'])
+        self.assertIsNotNone(data['analysed_sst'], "Skipping of not in dataset failed.")
+
+    def test_skipping_not_2D_variables(self):
+        """Test print error message if variable is not in dataset."""
+        data = extract_slice(self.file_path, variables=['lat'])
+        with self.assertRaises(KeyError):
+            data['lat']
+
+
+class TestCreateNetcdf(unittest.TestCase):
+    def setUp(self):
+        self.data = get_data_dict()
+        self.dest_path = get_dest_path()
+
+    def tearDown(self):
+        if os.path.exists(self.dest_path):
+            os.remove(self.dest_path)
+
+    def test_create_netcdf(self):
+        dest_path = self.dest_path
+        data = self.data
+        create_stack_file(data, dest_path)
+        self.assertTrue(os.path.exists(dest_path), "File {} not created.".format(dest_path))
+
+class TestCreateNetCDFStructure(unittest.TestCase):
+    def setUp(self):
+        self.data = get_data_dict()
+        self.dest_path = get_dest_path()
+        create_stack_file(self.data, self.dest_path)
+
+    def tearDown(self):
+        if os.path.exists(self.dest_path):
+            os.remove(self.dest_path)
+
+    def test_lat_lon_dimensions_filled(self):
+        with Dataset(self.dest_path, 'r', format="NETCDF4") as dataset:
+            dim = dataset.dimensions
+            self.assertTrue(dim['lat'].size > 0)
+            self.assertTrue(dim['lon'].size > 0)
+
+    def test_variables_in_dataset(self):
+        with Dataset(self.dest_path, 'r', format="NETCDF4") as dataset:
+            variables = self.data.keys()
+            for var in variables:
+                if var != 'dimensions':
+                    self.assertTrue(var in dataset.variables.keys(), "Variable {} not in dataset.".format(var))
+
+
+class TestWriteNetcdf(unittest.TestCase):
+    def setUp(self):
+        self.data = get_data_dict()
+        self.dest_path = get_dest_path()
+        create_netcdf_file(self.data, self.dest_path)
+
+    def tearDown(self):
+        if os.path.exists(self.dest_path):
+            os.remove(self.dest_path)
+
+    def test_fiel_not_exists(self):
+
+        not_exists = get_dest_path(f_path='not_exists.nc')
+
+        with self.assertRaises(IOError):
+            write_slice(self.data, not_exists)
+
+    def test_write_variable(self):
+
+        msg = "Variable {} is empty in dataset."
+
+        write_slice(self.data, self.dest_path)
+        with Dataset(self.dest_path, 'r', format="NETCDF4") as dataset:
+            variables = self.data.keys()
+            for var in variables:
+                if var != 'dimensions':
+                    self.assertTrue(dataset.variables[var].size > 0, msg.format(var))
+
+    def test_time_dimension_is_one(self):
+
+        msg = "Time dimension size is {} not 1."
+
+        write_slice(self.data, self.dest_path)
+        with Dataset(self.dest_path, 'r', format="NETCDF4") as dataset:
+            time_dimension = dataset.dimensions['time']
+            self.assertTrue(time_dimension.size == 1, msg.format(time_dimension.size))
+
+    def test_time_dimension_is_three(self):
+        msg = "Time dimension size is {} not {}."
+
+        times = 3
+
+        for step in range(0,times):
+            write_slice(self.data, self.dest_path)
+
+        with Dataset(self.dest_path, 'r', format="NETCDF4") as dataset:
+            time_dimension = dataset.dimensions['time']
+            self.assertTrue(time_dimension.size == times, msg.format(time_dimension.size, times))
+
+class TestStacking(unittest.TestCase):
+    def setUp(self):
+        self.inputs = get_inputs(TEST_FOLDER)
+        self.empty_dir = tempfile.mkdtemp()
+        self.output = get_dest_path()
+
+    def test_try_empty_inputs(self):
+        stacking([], VARIABLES, self.output)
+
+
+    def test_number_stack_slices_same_as_input(self):
+        """test number of time slices equal to number of files"""
+
+        with Dataset(self.output, 'r') as stack:
+            slices = stack.dimensions['time']
+            self.assertEqual(len(slices), 7)
+
 if __name__ == '__main__':
     unittest.main()
