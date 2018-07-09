db4e7999d38e8a37babded6cada762a65db2e883
Julius Schröder <julius-schroeder@posteo.de>
2018-07-09 16:31:16 +0100

    add generator to iterate over slices in cube

--- src/coresyf_data_cube_masking/masking.py
+++ src/coresyf_data_cube_masking/masking.py
@@ -5,16 +5,51 @@ and applie this to all slices.
 
 """
 
-import netCDF4
+import logging
+import numpy as np
 
 
-def get_slice(cube):
-    """Short summary.
+def Slices(cube, dim="date"):
+    """Create interator for `dim` dimension by using `get_slice` function.
+
+    Attention!
+    The `dim` deimension to iterate over must by on the first position in
+    `variabe.shape` tuple.
+
+    Parameters
+    ----------
+    cube : netCDF4 Dataset handle
+        Open cube file handle.
+    dim : type
+        Dimension to interate.
+
+    Returns
+    -------
+    interator
+        Interate over `dim` dimension.
+
+    """
+
+    try:
+        stop = len(cube.dimensions[dim])
+    except KeyError as e:
+        msg = "Dimension {} is no a cube dimension."
+        e(msg.format(dim))
+
+    for dim_ids in range(0, stop):
+        yield get_slice(cube, dim_ids)
+
+
+def get_slice(cube, dim_ids):
+    """Returns slice dictionary at index `dim_ids` from a three dimensional cube.
 
     Parameters
     ----------
     cube : netCDF file handle
         File handle to read from.
+
+    dim_ids : int
+        Slice to select from `dim`.
 
     Returns
     -------
@@ -24,5 +59,11 @@ def get_slice(cube):
     """
 
     slice = {}
+    for name, var in cube.variables.items():
+        if var.ndim == 3:
+            data = var[dim_ids, :, :]
+            slice[name] = data
 
     return slice
+
+
