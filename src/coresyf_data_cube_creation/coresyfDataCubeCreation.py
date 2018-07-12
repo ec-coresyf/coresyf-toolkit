faf5e55ae0984a2f14e9898686d39ab6225124d2
Julius Schröder <julius-schroeder@posteo.de>
2018-07-12 16:18:54 +0100

    refactore data cube creation to decrease

--- src/coresyf_data_cube_creation/coresyfDataCubeCreation.py
+++ src/coresyf_data_cube_creation/coresyfDataCubeCreation.py
@@ -33,6 +33,7 @@ from netCDF4 import Dataset
 
 from coresyftools.tool import CoReSyFTool
 
+
 def get_inputs(folder, pattern="*.nc"):
     """This searching for input files in folder matching pattern parameter
     returns list of path.
@@ -58,7 +59,7 @@ def get_inputs(folder, pattern="*.nc"):
 
     search_path = os.path.join(folder, pattern)
     inputs_iter = glob.iglob(search_path)
-    inputs = sorted(inputs_iter) # sort by file name
+    inputs = sorted(inputs_iter)  # sort by file name
 
     if inputs:
         logging.info("Fount {} inputs in {}.".format(len(inputs), folder))
@@ -66,6 +67,7 @@ def get_inputs(folder, pattern="*.nc"):
     else:
         msg = "No files found in {}! Search pattern are {}."
         raise IOError(msg.format(folder, search_path))
+
 
 def sorted_inputs(paths, key="date_created"):
     """Return one by date sorted list of paths as tuples like (path, date).
@@ -76,7 +78,8 @@ def sorted_inputs(paths, key="date_created"):
 
     list of tuple
         Sorted list of tuples holding input files path as string and date as integar e.g. (path, date).
-        List ist sorted afer date. Date is integar in dayes based on January 1 of year 1 (see date.toordinal() for more info).
+        List ist sorted afer date. Date is integar in dayes based on January 1 of year 1
+        (see date.toordinal() for more info).
 
     """
     inputs = []
@@ -90,13 +93,13 @@ def sorted_inputs(paths, key="date_created"):
         else:
             with dataset:
                 try:
-                  date_attr = dataset.getncattr(key)
+                    date_attr = dataset.getncattr(key)
                 except AttributeError as e:
-                  logging.warning("No date_created attribut found in input dataset, file name order to sort.")
-                  logging.debug(e)
-                  date = i
+                    logging.warning("No date_created attribut found in input dataset, file name order to sort.")
+                    logging.debug(e)
+                    date = i
                 else:
-                  date = dateutil.parser.parse(date_attr).toordinal()
+                    date = dateutil.parser.parse(date_attr).toordinal()
 
         item = (path, date)
         inputs.append(item)
@@ -104,96 +107,14 @@ def sorted_inputs(paths, key="date_created"):
     # sort list after date attribut
     return sorted(inputs, key=lambda item: item[1], reverse=True)
 
-def extract_slice(file_path, variables=None):
-    """Extract data from dataset by given variables.
 
-    This gets a netCDF file with spatial data as input file. As default all available variables will be extract from dataset, but optional the function get also a list of data variables to extract.
-
-    Parameters
-    ----------
-
-    file_path: str
-        Path to netCDF file.
-
-    variables: list
-        List of variables to extract from dataset. Default all variables will by extract.
-
-    Returns
-    -------
-    data: dict
-        Dictionary of extracted data. Variables names used as keys. Data are numpy.ma.MaskedArray instances. Additional dimensions key is used to save latitude and longitude dimensions.
-
-    Example
-    -------
-    > # Extract SST data from GHRSST netCDF4 file.
-    > extract_var = ["mask", "analysed_sst", "analysis_error"]
-    > extract_data("data/ghrsst.nc", extract_var)
-
-    """
-
-
-    data = {
-        'date': None,
-        'dimensions': {
-            'lat': None,
-            'lon': None,
-        },
-        'variables': {
-        }
-    }
-
-    try:
-        dataset = Dataset(file_path, "r", format="NETCDF4")
-    except IOError as e:
-        raise e
-
-    lat = dataset.variables["lat"][:]
-    lon = dataset.variables["lon"][:]
-
-    data['dimensions']['lat'] = lat
-    data['dimensions']['lon'] = lon
-
-    # read date and write in dict
-    try:
-      date_attr = dataset.date_created
-    except AttributeError as e:
-      logging.warning("No date_created attribut found in input dataset.")
-      logging.debug(e)
-    else:
-      date = dateutil.parser.parse(date_attr)
-      data['date'] = date.toordinal()
-
-    # filter by variable
-    if not variables:
-        keys = dataset.variables.keys()
-        variables = [var for var in keys if dataset.variables[var].ndim == 2] # only 2D variables
-
-    for var in variables:
-        try:
-            set = dataset.variables[var][:, :]
-        except KeyError:
-            msg = 'Variable {} is not in dataset.'
-            logging.debug(KeyError(msg.format(var)))
-        except ValueError:
-            msg = 'Variable {} number of dimension < 2.'
-            logging.debug(ValueError(msg.format(var)))
-        except IndexError as e:
-            raise e
-        else:
-            data['variables'][var] = set
-
-    dataset.close()
-
-    return data
-
-def create_stack(slice, ds_path):
+def create_stack(template_file, ds_path, variables):
     """This open a NetCDF4 file and creates basic stack structure.
 
     Parameters
     ----------
 
-    slice: dict
-        Dictionary holding dimmensions information.
+    template: open netCDF4 handle
 
     ds_path: string
         Destination Path for stack file.
@@ -205,68 +126,48 @@ def create_stack(slice, ds_path):
 
     """
 
-    dimensions = slice['dimensions']
-    variables = slice['variables']
+    dim_stacking = "date"
 
-    stacking_dimmension = "date"
-    stack_dimmensions = (stacking_dimmension,'lat', 'lon',)
-
+    # open target file
     try:
         stack = Dataset(ds_path, 'w', format="NETCDF4")
     except IOError:
         raise
     else:
+        template = Dataset(template_file, "r")
+        # use first input dataset as template
 
-        stack.createDimension(stacking_dimmension, None)
-        stack.createVariable(stacking_dimmension, "i4", (stacking_dimmension,))
+        temp_dim_lat = template.dimensions["lat"]
+        temp_dim_lon = template.dimensions["lon"]
 
-        # create and fill additional dimmensions (e.g. lat, lon)
-        for dim, dim_data in dimensions.items():
-            if len(dim_data):
-                stack.createDimension(dim, len(dim_data))
-            else:
-                stack.createDimension(dim, None) # add as unlimited dimension
+        # create dimmensions
+        stack.createDimension(dim_stacking, None)
+        stack.createDimension("lat", len(temp_dim_lat))
+        stack.createDimension("lon", len(temp_dim_lon))
 
-            dim_var = stack.createVariable(dim, dim_data.dtype, (dim,))
-            dim_var[:] = dim_data
+        # create variable in variables in stack file
+        stack.createVariable(dim_stacking, "i4", (dim_stacking,))
 
-        for var, var_data in variables.items():
-            stack.createVariable(var, var_data.dtype, stack_dimmensions, zlib=True)
-    return stack
+        temp_var_lat = template.variables["lat"]
+        temp_var_lon = template.variables["lon"]
 
-def write_slice(slice, stack, index):
-    """
-    This write slice to a netCDF file to create a stack.
+        stack_lat = stack.createVariable("lat", temp_var_lat.datatype, ("lat",))
+        stack_lon = stack.createVariable("lon", temp_var_lon.datatype, ("lon",))
 
-    Slice is an numpy array holding the data. The destination file is a path in string
-    by the ds_path. Use index parameter to specify the position in the stack.
+        stack_lat[:] = temp_var_lat[:]
+        stack_lon[:] = temp_var_lon[:]
 
+        # create data Variables
+        for name in variables:
+            var = template.variables[name]
+            stack_var = stack.createVariable(name, var.datatype, ("date", "lat", "lon"), zlib=True)
 
-    Parameters
-    ----------
+            # copy meta data for this variables
+            stack_var.setncatts({k: var.getncattr(k) for k in var.ncattrs()})
 
-    slice: dict
-        Dictionary of numpy arrays and variable names as Keys.
+        template.close()
+        return stack
 
-    stack: file object
-        File handel to stack file.
-
-    index: int
-        Position of slice in data stack.
-
-    Side effect
-    -------
-    Write slice to file stacked by index.
-
-    Example
-    -------
-
-    """
-    variables = slice['variables']
-
-    for var, data in variables.items():
-        var_data = stack.variables[var]
-        var_data[index, :, :] = data
 
 def stacking(inputs, variables, output):
     """This loob over inputs, extract data, write slices to file.
@@ -291,37 +192,25 @@ def stacking(inputs, variables, output):
     -------
     """
 
-    print inputs
+    logging.info('Create {} file.'.format(output))
+    try:
+        stack = create_stack(inputs[0][0], output, variables)
+    except EnvironmentError as e:
+        logging.error("Can't create {}".format(output))
+        logging.debug((os.strerror(e.errno)))
+        raise e
 
-    for count, input__ in enumerate(inputs):
+    for index, input__ in enumerate(inputs):
 
-        input_path = input__[0] # unpack input tuple
-
+        input_path = input__[0]  # unpack input tuple
         logging.info('Extracting data from {}'.format(input_path))
-        slice = extract_slice(input_path, variables=variables)
 
-        # run masking
-        # TODO: fix masking to zero and one
-        # data = masked_by_flags(data, mask_var="mask", flags=[2,5,9,13])
+        with Dataset(input_path, "r") as input:
+            for name in variables:
+                in_var = input.variables[name]
+                stack.variables[name][index, :, :] = in_var[:]
 
-        if os.path.exists(output) is False:
-            logging.info('Create {} file.'.format(output))
-
-            try:
-                stack = create_stack(slice, output)
-            except EnvironmentError as e:
-                logging.error("Can't create {}".format(output))
-                logging.debug((os.strerror(e.errno)))
-                raise e
-
-        try:
-            write_slice(slice, stack, index=count)
-        except EnvironmentError as e:
-            os.remove(output)
-
-            logging.error("Can't write to {}".format(output))
-            logging.debug((os.strerror(e.errno)))
-            raise e
+    stack.close()
 
 
 class CoReSyFDataCubeCreation(CoReSyFTool):
--- src/coresyf_data_cube_creation/tests/test_data_cube_creation.py
+++ src/coresyf_data_cube_creation/tests/test_data_cube_creation.py
@@ -9,40 +9,31 @@ import os
 
 from netCDF4 import Dataset
 
-from coresyf_data_cube_creation.coresyfDataCubeCreation import get_inputs
-from coresyf_data_cube_creation.coresyfDataCubeCreation import extract_slice
-from coresyf_data_cube_creation.coresyfDataCubeCreation import create_stack
-from coresyf_data_cube_creation.coresyfDataCubeCreation import write_slice
-from coresyf_data_cube_creation.coresyfDataCubeCreation import stacking
-from coresyf_data_cube_creation.coresyfDataCubeCreation import sorted_inputs
+from coresyfDataCubeCreation import get_inputs
+from coresyfDataCubeCreation import create_stack
+from coresyfDataCubeCreation import stacking
+from coresyfDataCubeCreation import sorted_inputs
 
 
 # Change if test data is changed
-TEST_FOLDER = os.path.join('coresyf_data_cube_creation',
-        'test_data',
-        'GHRSST',)
+TEST_FOLDER = os.path.join(
+    'test_data',
+    'GHRSST',)
 
-TEST_DATA = os.path.join('coresyf_data_cube_creation',
-        'test_data',
-        'GHRSST',
-        '20170101090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc')
+TEST_DATA = os.path.join(
+    'test_data',
+    'GHRSST',
+    '20170101090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc')
 
-VARIABLES = ['mask', 'analysed_sst', 'analysis_error']
+VARIABLES = ['analysed_sst', 'analysis_error', 'mask']
 
 
 # helper functions
-
-def get_data_dict():
-    """Return data dict by using extract_data()"""
-    file_path = os.path.abspath(TEST_DATA)
-    return extract_slice(file_path, variables=VARIABLES)
 
 def get_dest_path(f_path='output.nc'):
     tempdir = tempfile.mkdtemp()
     return os.path.join(tempdir, f_path)
 
-def create_netcdf_file(data, dest_path):
-    create_stack_file(data, dest_path)
 
 class TestGetInputs(unittest.TestCase):
     def setUp(self):
@@ -80,127 +71,23 @@ class TestSortedInputs(unittest.TestCase):
     def test_key_attribut_not_found(self):
         inputs = sorted_inputs(self.inputs, key="not_found_attribut")
         inputs_range = range(inputs[-1][1], inputs[0][1])
-        self.assertEqual(inputs_range, range(0,6))
+        self.assertEqual(inputs_range, range(0, 6))
 
-class TestExtractData(unittest.TestCase):
+
+class CreateStack(unittest.TestCase):
     def setUp(self):
-        self.file_path = os.path.abspath(TEST_DATA)
+        self.in_file = TEST_DATA
+        self.ds_path = get_dest_path()
+        self.variables = VARIABLES
 
-    def test_get_date_created(self):
-        """This tests getting date created from input dataset"""
-        data = extract_slice(self.file_path)
-        self.assertEqual(data['date'], 736333)
+    def test_all_variables(self):
+        stack = create_stack(
+            self.in_file,
+            self.ds_path,
+            self.variables
+        )
+        self.assertIs(len(stack.variables), len(VARIABLES))
 
-    def test_dimensions(self):
-        """This tests if data dict has dimensions."""
-
-        data = extract_slice(self.file_path)
-        self.assertIsNotNone(data['dimensions']['lat'], "Latitude dimension is undefined!")
-        self.assertIsNotNone(data['dimensions']['lon'], "Longitude dimension is undefined")
-        self.assertTrue(data['dimensions']['lat'].size > 0, "Latitude dimension is empty!")
-        self.assertTrue(data['dimensions']['lon'].size > 0, "Longitude dimension is empty!")
-
-    def test_one_variables(self):
-        """This tests if data dict has variables."""
-        data = extract_slice(self.file_path, variables=['analysed_sst'])
-        self.assertIsNotNone(data['variables']['analysed_sst'], "Variable is not in data dict!")
-        self.assertTrue(data['variables']['analysed_sst'].size > 0, "Variable is empty!")
-
-    def test_extract_variables_list(self):
-        """This tests if data dict has variables."""
-        data = extract_slice(self.file_path, variables=['analysed_sst','analysis_error'])
-        self.assertIsNotNone(data['variables']['analysed_sst'], "Variable is not in data dict!")
-        self.assertTrue(data['variables']['analysed_sst'].size > 0, "Variable is empty!")
-
-    def test_variables_are_2D(self):
-        """This tests if data dict has variables."""
-        data = extract_slice(self.file_path)
-        self.assertTrue(data['variables']['analysed_sst'].ndim == 2, "Variable is not 2D!")
-
-    def test_variable_not_in_dataset(self):
-        """Test print error message if variable is not in dataset."""
-        data = extract_slice(self.file_path, variables=['not_in_dataset'])
-        with self.assertRaises(KeyError):
-            data['not_in_dataset']
-
-    def test_skipping_not_in_dataset_variables(self):
-        """Test print error message if variable is not in dataset."""
-        data = extract_slice(self.file_path, variables=['not_in_dataset', 'analysed_sst'])
-        self.assertIsNotNone(data['variables']['analysed_sst'], "Skipping of not in dataset failed.")
-
-    def test_skipping_not_2D_variables(self):
-        """Test print error message if variable is not in dataset."""
-        data = extract_slice(self.file_path, variables=['lat'])
-        with self.assertRaises(KeyError):
-            data['lat']
-
-
-class TestCreateStack(unittest.TestCase):
-    def setUp(self):
-        self.data = get_data_dict()
-        self.dest_path = get_dest_path()
-
-    def tearDown(self):
-        if os.path.exists(self.dest_path):
-            os.remove(self.dest_path)
-
-    def test_create_netcdf(self):
-        dest_path = self.dest_path
-        data = self.data
-        stack = create_stack(data, dest_path)
-        self.assertTrue(os.path.exists(dest_path), "File {} not created.".format(dest_path))
-        stack.close()
-
-class TestStackStructure(unittest.TestCase):
-    def setUp(self):
-        data = get_data_dict()
-        self.variables = data['variables']
-        self.dimmensions = data['dimensions']
-        self.dest_path = get_dest_path()
-        self.stack = create_stack(data, self.dest_path)
-
-    def tearDown(self):
-        self.stack.close()
-        if os.path.exists(self.dest_path):
-            os.remove(self.dest_path)
-
-    def test_lat_lon_dimensions_filled(self):
-        dim = self.stack.dimensions
-        self.assertTrue(dim['lat'].size > 0)
-        self.assertTrue(dim['lon'].size > 0)
-
-    def test_variables_in_dataset(self):
-        msg = "Variable {} not in dataset."
-        for var in self.stack.variables:
-            self.assertTrue(var in self.stack.variables.keys(), msg.format(var))
-
-
-class TestWriteSlice(unittest.TestCase):
-    def setUp(self):
-        self.data = get_data_dict()
-        self.dest_path = get_dest_path()
-        self.stack = create_stack(self.data, self.dest_path)
-        self.variables = self.data["variables"]
-
-    def tearDown(self):
-        self.stack.close()
-        if os.path.exists(self.dest_path):
-            os.remove(self.dest_path)
-
-    def test_write_variable(self):
-        msg = "Variable {} is empty in dataset."
-        write_slice(self.data, self.stack, index=1)
-        for var in self.variables:
-            self.assertTrue(self.stack.variables[var].size > 0, msg.format(var))
-
-    def test_time_dimension_is_three(self):
-        msg = "Time dimension size is {} not {}."
-        times = 3
-        for step in range(0,times):
-            write_slice(self.data, self.stack, index=step)
-
-        time_dimension = self.stack.dimensions['date']
-        self.assertTrue(time_dimension.size == times, msg.format(time_dimension.size, times))
 
 class TestStacking(unittest.TestCase):
     def setUp(self):
@@ -221,6 +108,7 @@ class TestStacking(unittest.TestCase):
             var_in = stack.variables.keys()
             contains = all(elem in var_in for elem in VARIABLES)
             self.assertTrue(contains)
+
 
 if __name__ == '__main__':
     unittest.main()
