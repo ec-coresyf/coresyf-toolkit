c064663352b132ef382c8414534b6ec814682684
Nuno Costa <nuno.costa@deimos.com.pt>
2018-07-13 14:56:55 +0100

    asks for input and output prefixes

--- src/manifest_argsparser/argparse.py
+++ src/manifest_argsparser/argparse.py
@@ -5,14 +5,36 @@ class ArgumentParser:
 
     def __init__(self, *args, **kwargs):
         self.args = {}
+        self.input_options = set()
+        self.output_options = set()
 
     def __getattr__(self, name):
         pass
+    
+    def get_arguments_list(self):
+        for _id, arg in self.args.items():
+            yield (_id, arg['help'])
+    
+    def set_inputs(self, inputs):
+        self.input_options = set(inputs)
+    
+    def set_outputs(self, outputs):
+        self.output_options = set(outputs)
 
     def add_argument(self, *opts, **kwargs):
         longest_opts = max(opts, key=len)
         _id = longest_opts[2:]
         self.args[_id] = kwargs
+
+    def _entry(self, _id, arg, in_out=False):
+        entry = {
+                "identifier": _id,
+                "name": _id,
+                "description": arg.get('help', ''),
+        }
+        if in_out:
+           entry['collection'] = arg.get('nargs') != 1
+        return entry
 
     def manifest(self):
         manifest = {
@@ -20,32 +42,23 @@ class ArgumentParser:
             "parameters": [],
             "outputs": []
         }
-        for _id, arg in self.args.items():
-            entry = {
-                    "identifier": _id,
-                    "name": _id,
-                    "description": arg.get('help', ''),
-            }
-            if 'input' in _id or 'output' in _id:
-                entry['collection'] = 'nargs' in arg and arg['nargs'] != 1
-            if 'input' in _id: 
-                manifest['inputs'].append(entry)
-            elif 'output' in _id:
-                manifest['outputs'].append(entry)
-            else:
-                if 'default' in arg:
-                    entry['default'] = arg['default']
-                if 'choices' in arg:
-                    entry['options'] = arg['choices']
-                if 'type' in arg:
-                    entry['type'] = arg['type']
-                manifest['parameters'].append(entry)
+        for _id in self.input_options:
+            arg = self.args[_id]
+            manifest['inputs'].append(self._entry(_id, arg, True))
+        for _id in self.output_options:
+            arg = self.args[_id]
+            manifest['outputs'].append(self._entry(_id, arg, True))
+        outputs = set(self.args.keys()) - self.input_options - self.output_options
+        for _id in outputs:
+            arg = self.args[_id]
+            entry = self._entry(_id, arg) 
+            if 'default' in arg:
+                entry['default'] = arg['default']
+            if 'choices' in arg:
+                entry['options'] = arg['choices']
+            if 'type' in arg:
+                entry['type'] = arg['type']
+            manifest['parameters'].append(entry)
         return manifest
 
-    def generate_manifest(self, tool_dir, tool_name):
-        manifest_json = json.dumps(self.manifest(), indent=4)
-        manifest_file_name = tool_name + '.manifest.json'
-        manifest_file_path = Path(tool_dir) / manifest_file_name
-        with open(str(manifest_file_path), 'w') as manifest_file:
-            manifest_file.write(manifest_json)
-
+  
--- src/manifest_argsparser/generate_manifest.py
+++ src/manifest_argsparser/generate_manifest.py
@@ -2,10 +2,15 @@
 import re
 import glob
 import os
+import json
 import click
 from pathlib import Path
+import logging
 from argparse import ArgumentParser
-ARG_ADDITION_REGEX = re.compile(r'\s*parser.add_argument\(.*\)\n')
+ARG_ADDITION_REGEX = re.compile(r'.*parser.add_argument\(.*\)')
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def parse_argparser_in_file(filename):
     with open(filename) as source_file:
@@ -13,9 +18,18 @@ def parse_argparser_in_file(filename):
         matches = re.findall(ARG_ADDITION_REGEX, source)
         return matches
 
+def write_manifest(manifest, tool_dir, tool_name):
+    manifest_json = json.dumps(manifest, indent=4)
+    manifest_file_name = tool_name + '.manifest.json'
+    manifest_file_path = Path(tool_dir) / manifest_file_name
+    with open(str(manifest_file_path), 'w') as manifest_file:
+        manifest_file.write(manifest_json)
+
 def create_argument_parser(filename):
     parser = ArgumentParser()
+    logger.info('Parsing...')
     for line in parse_argparser_in_file(filename):
+        logger.info('Parsing %s.', line)
         exec(line.strip())
     return parser
 
@@ -26,8 +40,17 @@ def create_argument_parser(filename):
 def create_manifest(runfile, tool_dir=None):
     if tool_dir is None:
         tool_dir = Path(runfile).parent
-    name = Path(runfile).parent.name
-    create_argument_parser(runfile).generate_manifest(tool_dir, name)
+    name = Path(runfile).resolve().parent.name
+    argparser = create_argument_parser(runfile)
+    click.echo('Parsed arguments:')
+    for prefix, _help in argparser.get_arguments_list():
+        click.echo('{}: {}'.format(prefix, _help))
+    input_options = raw_input('Which options are inputs? Write their prefixes separeted by space.\n')
+    output_options = raw_input('Which options are outputs? Write their prefixes separated by space.\n')
+    argparser.set_inputs(input_options.split())
+    argparser.set_outputs(output_options.split())
+    manifest = argparser.manifest()
+    write_manifest(manifest, tool_dir, name)
 
 if __name__ == '__main__':
     create_manifest()
