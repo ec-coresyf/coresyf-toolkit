abd223195db1c25c065668e4a427ed59ea287e41
Julius Schröder <julius-schroeder@posteo.de>
2018-07-13 10:18:24 +0100

    make tool runable by commandline

--- src/coresyf_data_cube_masking/coresyfDataCubeMasking.py
+++ src/coresyf_data_cube_masking/coresyfDataCubeMasking.py
@@ -1,23 +1,21 @@
 # coding: utf-8
 # ! python2
 
+from coresyftools.tool import CoReSyFTool
 from lib import masking
+from netCDF4 import Dataset
+
 
 class CoReSyFDataCubeMasking(CoReSyFTool):
 
     def run(self, bindings):
         input = bindings['Ssource']
-        flags = list(bindings['flags'])
+        flags = list(bindings['flags'].split(","))
+        target = bindings['Ttarget']
 
         with Dataset(input, 'r', format="NETCDF4") as in_cube:
-
-            # for s in masking.Slices(cube, dim="date"):
-            #    masking.mask_by_flags(s, flags, name="mask")
             mask = masking.aggregate_mask(in_cube, flags, dim="date", mask_var="mask")
 
-        with Dataset(Ttarget, "w", format="NETCDF4") as out_cube:
-            # TODO: create copy of input cube and change variables to masked version
+        with Dataset(target, "w", format="NETCDF4") as out_cube:
             masking.masking_cube(cube, mask)
-        # parse Parameters
-        # masking data cube
-        pass
+            # TODO: create copy of input cube and change variables to masked version
--- src/coresyf_data_cube_masking/examples.sh
+++ src/coresyf_data_cube_masking/examples.sh
@@ -1,3 +1,3 @@
 #PASSING EXECUTION
 #Example of a passing execution
-./run 
+./run --Ssource test_data/cube.nc --flags "2,5,9,13" --Ttarget test_data/cube_masked.nc
