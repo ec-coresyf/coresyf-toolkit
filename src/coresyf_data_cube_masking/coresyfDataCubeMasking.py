4e6b1cad3c383a4ef262c875353f5429c7c73462
Julius Schröder <julius-schroeder@posteo.de>
2018-07-19 09:18:56 +0100

    add fill default fill value for missing/masked elements
    The fill value is used to replace masked values on variable export.

--- src/coresyf_data_cube_masking/coresyfDataCubeMasking.py
+++ src/coresyf_data_cube_masking/coresyfDataCubeMasking.py
@@ -13,9 +13,27 @@ class CoReSyFDataCubeMasking(CoReSyFTool):
         flags = list(bindings['flags'].split(","))
         target = bindings['Ttarget']
 
-        with Dataset(input, 'r', format="NETCDF4") as in_cube:
-            mask = masking.aggregate_mask(in_cube, flags, dim="date", mask_var="mask")
+        in_cube = Dataset(input, 'r', format="NETCDF4")
+        mask = masking.aggregate_mask(in_cube, flags, dim="date", mask_var="mask")
 
-        with Dataset(target, "w", format="NETCDF4") as out_cube:
-            masking.masking_cube(cube, mask)
-            # TODO: create copy of input cube and change variables to masked version
+        out_cube = Dataset(target, "w", format="NETCDF4")
+
+        # copy in cube to out_cube
+        for dname, the_dim in in_cube.dimensions.iteritems():
+            out_cube.createDimension(dname, len(the_dim) if not the_dim.isunlimited() else None)
+
+        for v_name, varin in in_cube.variables.iteritems():
+            outVar = out_cube.createVariable(v_name, varin.datatype, varin.dimensions, fill_value=-999)
+
+            # Copy variable attributes
+            outVar.setncatts({k: varin.getncattr(k) for k in varin.ncattrs()})
+
+            outVar[:] = varin[:]
+
+        masking.masking_cube(out_cube, mask)
+
+        in_cube.close()
+        out_cube.close()
+
+
+        # TODO: create copy of input cube and change variables to masked version
