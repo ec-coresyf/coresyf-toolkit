86d5e455f72d6c8d27259cfb47f8d39255143e28
Unknown <r.scarrott@ucc.ie>
2018-07-19 10:46:44 +0100

    Hypertemp: Separability Decision tool - Add baseline examples file

---
+++ src/coresys_OHMA_Separability_Decision/examples.sh
@@ -0,0 +1,3 @@
+#PASSING EXECUTION
+#Passing execution of the designated textfile (where Sep. values are recorded), and the output textfile (decision info), no parameter needed.
+./run --Ssource test_data/2011_SeparabilityMeasures.txt --Ttarget test_data/2011_SeparabilityMeasuresDecision.txt
