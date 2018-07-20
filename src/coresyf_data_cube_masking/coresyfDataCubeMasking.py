db26dd975e3ccf3dce325a2107cd480020f211f3
Julius Schröder <julius-schroeder@posteo.de>
2018-07-20 16:59:21 +0100

    fix with statments

--- src/coresyf_data_cube_masking/coresyfDataCubeMasking.py
+++ src/coresyf_data_cube_masking/coresyfDataCubeMasking.py
@@ -13,8 +13,7 @@ class CoReSyFDataCubeMasking(CoReSyFTool):
         flags = list(bindings['flags'].split(","))
         target = bindings['Ttarget']
 
-        with Dataset(input, 'r', format="NETCDF4") as in_cube,
-        Dataset(target, "w", format="NETCDF4") as out_cube:
+        with Dataset(input, 'r', format="NETCDF4") as in_cube, Dataset(target, "w", format="NETCDF4") as out_cube:
 
             mask = masking.aggregated_mask(in_cube, flags, dim="date", mask_var="mask")
             masking.masking_cube(in_cube, out_cube, mask)
