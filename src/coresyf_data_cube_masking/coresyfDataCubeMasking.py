33a71e5f9f692651a585d4a3c6b1b4eb65730c6d
Julius Schröder <julius-schroeder@posteo.de>
2018-07-20 16:57:02 +0100

    replace tow files handles by multible statements

--- src/coresyf_data_cube_masking/coresyfDataCubeMasking.py
+++ src/coresyf_data_cube_masking/coresyfDataCubeMasking.py
@@ -13,13 +13,10 @@ class CoReSyFDataCubeMasking(CoReSyFTool):
         flags = list(bindings['flags'].split(","))
         target = bindings['Ttarget']
 
-        in_cube = Dataset(input, 'r', format="NETCDF4")
-        mask = masking.aggregated_mask(in_cube, flags, dim="date", mask_var="mask")
-        out_cube = Dataset(target, "w", format="NETCDF4")
-        masking.masking_cube(out_cube, mask)
+        with Dataset(input, 'r', format="NETCDF4") as in_cube,
+        Dataset(target, "w", format="NETCDF4") as out_cube:
 
-        in_cube.close()
-        out_cube.close()
-
+            mask = masking.aggregated_mask(in_cube, flags, dim="date", mask_var="mask")
+            masking.masking_cube(in_cube, out_cube, mask)
 
         # TODO: create copy of input cube and change variables to masked version
