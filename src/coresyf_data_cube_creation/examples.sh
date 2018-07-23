f25c44a5aeec833f96be5244c34b31d9e0a6e547
Julius Schröder <julius-schroeder@posteo.de>
2018-07-23 11:46:45 +0100

    wirte results in test_data

--- src/coresyf_data_cube_creation/examples.sh
+++ src/coresyf_data_cube_creation/examples.sh
@@ -1,3 +1,3 @@
 #PASSING EXECUTION
 #Example of a passing execution
-./run --Ssource test_data/GHRSST --var "mask,analysed_sst" --Ttarget data_cube.nc
+./run --Ssource test_data/GHRSST --var "mask,analysed_sst" --Ttarget test_data/data_cube.nc
