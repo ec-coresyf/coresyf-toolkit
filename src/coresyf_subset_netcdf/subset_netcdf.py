113c88b1dfbdfa43c444c10199a0e1fd0a29375c
Julius Schröder <julius-schroeder@posteo.de>
2018-07-30 10:31:16 +0100

    change folder validaton error message

--- src/coresyf_subset_netcdf/subset_netcdf.py
+++ src/coresyf_subset_netcdf/subset_netcdf.py
@@ -146,11 +146,11 @@ class readable_dir(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         prospective_dir = values
         if not os.path.isdir(prospective_dir):
-            raise argparse.ArgumentTypeError("readable_dir:{0} is not a valid path".format(prospective_dir))
+            raise argparse.ArgumentTypeError("readable_dir:{0} is not a folder".format(prospective_dir))
         if os.access(prospective_dir, os.R_OK):
             setattr(namespace, self.dest, prospective_dir)
         else:
-            raise argparse.ArgumentTypeError("readable_dir:{0} is not a readable dir".format(prospective_dir))
+            raise argparse.ArgumentTypeError("readable_dir:{0} is not a readable folder".format(prospective_dir))
 
 
 if __name__ == '__main__':
