39717896d7f86d46570b951fccc55d76000bd902
Julius Schröder <julius-schroeder@posteo.de>
2018-08-03 16:24:33 +0100

    Update CLI interface, add new parameter and improve description

--- src/coresyf_data_cube_creation/examples.sh
+++ src/coresyf_data_cube_creation/examples.sh
@@ -1,3 +1,3 @@
 #PASSING EXECUTION
 #Example of a passing execution
-./run --Ssource test_data/GHRSST --var "mask,analysed_sst" --Ttarget test_data/data_cube.nc
+./run --Ssource test_data/ --data_name analysed_sst --mask_name mask --extension ".img" --Ttarget test_data/data_cube.nc
