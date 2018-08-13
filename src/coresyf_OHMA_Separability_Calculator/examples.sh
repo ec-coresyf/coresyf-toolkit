b1cf8d74cefa049d0caef7c23098898f1117e4f9
Unknown <r.scarrott@ucc.ie>
2018-08-13 18:13:04 +0100

    Hypertemp: Separability Calculator tool - add the examples.sh file
    Pretty self descriptive really...

---
+++ src/coresyf_OHMA_Separability_Calculator/examples.sh
@@ -0,0 +1,3 @@
+#PASSING EXECUTION
+#Passing execution of the designated folder containing the .xlxs files (where mean vectors and covariance matrices are recorded), and the output path), no parameters are needed as the script harvests everything it needs from the .xlxs files themselves.
+./run --Ssource test_data/ --Ttarget test_output/
